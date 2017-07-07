c0c25e949d748944848760e08286bb7665133898
rocco8773 <eteverson@gmail.com>
2017-07-07 16:44:13 -0700

    adding viewer directory

--- bapsflib/lapdhdf/files.py
+++ bapsflib/lapdhdf/files.py
@@ -1,10 +1,10 @@
 import h5py
 
 class File(h5py.File):
+
     def __init__(self, name, mode='r', driver=None, libver=None, userblock_size=None, swmr=False, **kwds):
         h5py.File.__init__(self, name, mode, driver, libver, userblock_size, swmr)
 
-    @property
     def listHDF_files(self):
         listHDF_files = []
         self.visit(listHDF_files.append)

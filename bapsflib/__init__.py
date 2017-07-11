9d6b31748d7d8b2190d108301c48ce6347d06b8a
rocco8773 <eteverson@gmail.com>
2017-07-11 14:31:02 -0700

    added comments and lapdhdf import to bapsflib __init__

---
+++ bapsflib/__init__.py
@@ -0,0 +1,16 @@
+# This file is part of the bapsflib package, a Python toolkit for the BaPSF group at UCLA.
+#
+# http://plasma.physics.ucla.edu/
+#
+# Copyright 2017 Erik T. Everson and contributors
+#
+# License:
+#
+
+'''
+    This is the bapsflib package, a Python toolkit for the BaPSF group at UCLA.
+
+    http://plasma.physics.ucla.edu/
+'''
+
+from . import lapdhdf

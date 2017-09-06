f9b04aae5b765c7a532175b7e83f47d2dfc36798
rocco8773 <eteverson@gmail.com>
2017-09-05 20:58:46 -0700

    modified conf.py in attempts to work around Readthedoc's issue with C dependent modules

--- docs/conf.py
+++ docs/conf.py
@@ -205,5 +205,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['PyQt', 'numpy', 'pandas']
+MOCK_MODULES = ['PyQt', 'numpy', 'pandas', 'h5py']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)

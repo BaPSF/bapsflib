3b518824547b337e3991bdcf1b370bcefc69d0ed
rocco8773 <eteverson@gmail.com>
2017-09-10 16:32:33 -0700

    initial commit for MANIFEST.in

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     description='A toolkit for handling data collected at BaPSF.',
     url='https://github.com/rocco8773/bapsflib',
     author='Erik T. Everson',
-    author_email='eteveson@gmail.com'
+    author_email='eteveson@gmail.com',
+    zip_safe=False
 )

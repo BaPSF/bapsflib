3cf513f52b6ffce3ea12760480eb93f875883147
rocco8773 <eteverson@gmail.com>
2017-09-10 12:55:01 -0700

    initial setup.py commit, modified .gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='bapsflib',
+    version='0.1.2.dev',
+    description='A toolkit for handling data collected at BaPSF.',
+    url='https://github.com/rocco8773/bapsflib',
+    author='Erik T. Everson',
+    author_email='eteveson@gmail.com'
+)

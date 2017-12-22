4b51fe222561598f440344c5efa6d7119c710515
rocco8773 <eteverson@gmail.com>
2017-12-21 23:38:40 -0800

    updated License in header docs

--- bapsflib/lapdhdf/__init__.py
+++ bapsflib/lapdhdf/__init__.py
@@ -5,7 +5,8 @@
 #
 # Copyright 2017 Erik T. Everson and contributors
 #
-# License:
+# License: Standard 3-clause BSD; see "LICENSES/LICENSE.txt" for full
+#   license terms and contributor agreement.
 #
 """
 The lapdhdf module contains the necessary tools to open, identify,

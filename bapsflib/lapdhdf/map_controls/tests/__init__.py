ec5306c9e5e9afb43a50bd3624d212ca449a2867
rocco8773 <eteverson@gmail.com>
2018-01-21 17:58:35 -0800

    initial commit for tests of the map_contorls module

---
+++ bapsflib/lapdhdf/map_controls/tests/__init__.py
@@ -0,0 +1,10 @@
+# This file is part of the bapsflib package, a Python toolkit for the
+# BaPSF group at UCLA.
+#
+# http://plasma.physics.ucla.edu/
+#
+# Copyright 2017-2018 Erik T. Everson and contributors
+#
+# License: Standard 3-clause BSD; see "LICENSES/LICENSE.txt" for full
+#   license terms and contributor agreement.
+#

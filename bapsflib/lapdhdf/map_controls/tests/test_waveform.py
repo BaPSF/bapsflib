9db813506838d293933626ae17346e21bd919075
rocco8773 <eteverson@gmail.com>
2018-01-28 17:32:27 -0800

    updated to use FauxHDFBuilder to generated the HDF5 file

--- bapsflib/lapdhdf/map_controls/tests/test_waveform.py
+++ bapsflib/lapdhdf/map_controls/tests/test_waveform.py
@@ -11,8 +11,10 @@
 # License: Standard 3-clause BSD; see "LICENSES/LICENSE.txt" for full
 #   license terms and contributor agreement.
 #
-from .tempwaveform import TemporaryWaveform
+#from .fauxwaveform import FauxWaveform
 from ..waveform import hdfMap_control_waveform
+
+from bapsflib.lapdhdf.tests.fauxhdfbuilder import FauxHDFBuilder
 
 import tempfile
 import h5py
@@ -24,14 +26,18 @@ class TestWaveform(ut.TestCase):
     N_CONFIGS = 1
 
     def setUp(self):
-        self.tempdir = tempfile.TemporaryDirectory(prefix='hdf-test_')
-        self.f = TemporaryWaveform(self.N_CONFIGS, dir=self.tempdir)
+        # self.tempdir = tempfile.TemporaryDirectory(prefix='hdf-test_')
+        # self.f = FauxWaveform(self.N_CONFIGS, dir=self.tempdir)
+        self.f = FauxHDFBuilder()
+        self.controls = self.f.modules['waveform']
+        self.controls.n_configs = self.N_CONFIGS
         self.cgroup = self.f['Raw data + config/Waveform']
         self.cmap = hdfMap_control_waveform(self.cgroup)
 
     def tearDown(self):
-        self.f.close()
-        self.tempdir.cleanup()
+        self.f.cleanup()
+        #self.f.close()
+        #self.tempdir.cleanup()
 
     def test_info(self):
         self.assertTrue(hasattr(self.cmap, 'info'))
@@ -54,7 +60,7 @@ class TestWaveform(ut.TestCase):
         self.assertTrue(self.cmap.has_command_list)
 
     def test_one_config_per_dataset(self):
-        if self.f.n_configs == 1:
+        if self.controls.n_configs == 1:
             self.assertTrue(self.cmap.one_config_per_dset)
         else:
             self.assertFalse(self.cmap.one_config_per_dset)
@@ -70,7 +76,7 @@ class TestWaveform(ut.TestCase):
         self.assertEqual(len(self.cmap.configs), self.N_CONFIGS)
 
         for config in self.cmap.configs:
-            self.assertIn(config, self.f.config_names)
+            self.assertIn(config, self.controls.config_names)
             self.assertIn('IP address', self.cmap.configs[config])
             self.assertIn('device name', self.cmap.configs[config])
             self.assertIn('command list', self.cmap.configs[config])

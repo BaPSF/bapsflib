e175b310168287023d65f83c0bc4a3744f11e9f8
rocco8773 <eteverson@gmail.com>
2018-01-31 16:51:08 -0800

    initial commit for test_hdfmappers.py

---
+++ bapsflib/lapdhdf/tests/test_hdfmappers.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# This file is part of the bapsflib package, a Python toolkit for the
+# BaPSF group at UCLA.
+#
+# http://plasma.physics.ucla.edu/
+#
+# Copyright 2017-2018 Erik T. Everson and contributors
+#
+# License: Standard 3-clause BSD; see "LICENSES/LICENSE.txt" for full
+#   license terms and contributor agreement.
+#
+import unittest as ut
+
+from . import FauxHDFBuilder
+
+from ..hdfmappers import hdfMap
+
+
+class TestHDFMap(ut.TestCase):
+    """
+    Test Case for :class:`~bapsflib.lapdhdf.hdfmappers.hdfMap`
+    """
+    def setUp(self):
+        self.f = FauxHDFBuilder()
+        self.map = hdfMap(self.f)
+
+    def tearDown(self):
+        self.f.cleanup()
+
+    def test_attribute_existence(self):
+        self.assertTrue(hasattr(self.map, 'msi'))
+        self.assertTrue(hasattr(self.map, 'digitizers'))
+        self.assertTrue(hasattr(self.map, 'main_digitizer'))
+        self.assertTrue(hasattr(self.map, 'controls'))
+        self.assertTrue(hasattr(self.map, 'unknowns'))
+        self.assertTrue(hasattr(self.map, 'hdf_version'))
+
+    def test_has_attributes(self):
+        self.assertTrue(self.map.has_msi_group)
+        self.assertTrue(self.map.has_data_group)
+        self.assertFalse(self.map.has_data_run_sequence)
+        self.assertFalse(self.map.has_digitizers)
+        self.assertFalse(self.map.has_controls)
+        self.assertFalse(self.map.has_unknown_data_subgoups)
+
+    def test_msi(self):
+        self.assertIsInstance(self.map.msi, dict)
+        self.assertEqual(len(self.map.msi), 0)
+
+    def test_digitizers(self):
+        self.assertIsInstance(self.map.digitizers, dict)
+        self.assertEqual(len(self.map.digitizers), 0)
+
+    def test_controls(self):
+        self.assertIsInstance(self.map.controls, dict)
+        self.assertEqual(len(self.map.controls), 0)
+
+    def test_unknowns(self):
+        self.assertIsInstance(self.map.unknowns, dict)
+        self.assertEqual(len(self.map.unknowns), 0)
+
+    def test_hdf_version(self):
+        self.assertEqual(self.map.hdf_version, '0.0.0')
+
+    def test_main_dgitizer(self):
+        self.assertIs(self.map.main_digitizer, None)
+
+
+if __name__ == '__main__':
+    ut.main()

27e91e7e26bbf3948f1047fda0009656937271cb
rocco8773 <eteverson@gmail.com>
2018-02-01 19:20:13 -0800

    FauxSixK: initial coding of self._update to write Groups and their attributes, still need to add Datasets

--- bapsflib/lapdhdf/map_controls/tests/fauxsixk.py
+++ bapsflib/lapdhdf/map_controls/tests/fauxsixk.py
@@ -9,15 +9,21 @@
 #   license terms and contributor agreement.
 #
 import h5py
+import random
 import math
 import numpy as np
+
+from datetime import datetime as dt
 
 
 class FauxSixK(h5py.Group):
     """
     Creates a Faux '6K Compumotor' Group in a HDF5 file.
     """
-    def __init__(self, id, n_configs=1, sn_size=100, **kwargs):
+    _MAX_CONFIGS = 4
+
+    def __init__(self, id, n_configs=1, n_motionlists=1,
+                 sn_size=100, **kwargs):
         # ensure id is for a HDF5 group
         if not isinstance(id, h5py.h5g.GroupID):
             raise ValueError('{} is not a GroupID'.format(id))
@@ -28,6 +34,8 @@ class FauxSixK(h5py.Group):
 
         # store number on configurations
         self._n_configs = n_configs
+        self._n_probes = n_configs
+        self._n_motionlists = n_motionlists if n_configs == 1 else 1
 
         # define number of shot numbers
         self._sn_size = sn_size
@@ -41,10 +49,23 @@ class FauxSixK(h5py.Group):
         return self._n_configs
 
     @n_configs.setter
-    def n_configs(self, val):
+    def n_configs(self, val: int):
         """Set number of waveform configurations"""
-        if val != self._n_configs:
+        if val != self._n_configs and 1 <= val <= self._MAX_CONFIGS:
             self._n_configs = val
+            self._n_probes = self._n_configs
+            if val > 1:
+                self._n_motionlists = 1
+            self._update()
+
+    @property
+    def n_motionlists(self):
+        return self._n_motionlists
+
+    @n_motionlists.setter
+    def n_motionlists(self, val: int):
+        if val != self._n_motionlists and self.n_configs == 1:
+            self._n_motionlists = val
             self._update()
 
     @property
@@ -69,8 +90,82 @@ class FauxSixK(h5py.Group):
         Updates control group structure (Groups, Datasets, and
         Attributes)
         """
-        self._config_names = []
+        # clear group before rebuild
+        self.clear()
 
-    def _set_attrs(self):
-        """Sets attributes for the control group and its sub-members"""
-        pass
+        self._config_names = []
+        self._probe_names = []
+        self._motionlist_names = []
+
+        # add probe sub-groups
+        # - define probe names
+        # - receptacle number
+        # - configuration name
+        # - create probe groups and sub-groups
+        # - define probe group attributes
+        for i in range(self.n_configs):
+            # define probe name
+            pname = 'probe{:02}'.format(i + 1)
+            self._probe_names.append(pname)
+
+            # define receptacle number
+            if self.n_configs == 1:
+                receptacle = random.randint(1, self._MAX_CONFIGS)
+            else:
+                receptacle = i + 1
+
+            # gather configuration names
+            self._config_names.append(receptacle)
+
+            # create probe group
+            probe_gname = 'Probe: XY[{}]: '.format(receptacle) + pname
+            self.create_group(probe_gname)
+            self.create_group(probe_gname + '/Axes[0]')
+            self.create_group(probe_gname + '/Axes[1]')
+
+            # set probe group attributes
+            self[probe_gname].attrs.update({
+                'Port': -99999,
+                'Probe': pname.encode(),
+                'Probe type': b'LaPD probe',
+                'Receptacle': receptacle
+            })
+
+        # add motionlist sub-groups
+        # - define motionlist names
+        # - create motionlist group
+        # - define motionlist group attributes
+        for i in range(self.n_motionlists):
+            # define motionlist name
+            ml_name = 'ml-{:04}'.format(i+1)
+            self._motionlist_names.append(ml_name)
+
+            # create motionlist group
+            ml_gname = 'Motion list: ' + ml_name
+            self.create_group(ml_gname)
+
+            # find divisible numbers of sn_size
+            sn_div = []
+            for j in range(self.sn_size):
+                if self.sn_size % (j + 1) == 0:
+                    sn_div.append(j + 1)
+
+            # set motionlist attributes
+            sn_div_index = random.randint(0, len(sn_div) - 1)
+            Nx = sn_div[sn_div_index]
+            Ny = int( self.sn_size / Nx)
+            timestamp = dt.now().strftime('%-m/%-d/%Y %-I:%M:%S %p')
+            self[ml_gname].attrs.update({
+                'Created date': timestamp.encode(),
+                'Grid center x': 0.0,
+                'Grid cneter y': 0.0,
+                'Delta x': 1.0,
+                'Delta y': 1.0,
+                'Nx': Nx,
+                'Ny': Ny,
+                'Motion list': ml_name.encode(),
+                'Data motion count': Nx * Ny,
+                'Motion count': -99999
+            })
+
+            # TODO: CREATE DATASET

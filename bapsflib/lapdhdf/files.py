9386a63ff35d4309f01a392d94f8251ae8ae1644
Erik Everson <eeverson@midas.pa.ucla.edu>
2018-02-15 14:07:52 -0800

    adding some_info attribute to File class

--- bapsflib/lapdhdf/files.py
+++ bapsflib/lapdhdf/files.py
@@ -47,6 +47,9 @@ class File(h5py.File):
         print('Begin HDF5 Quick Report:')
         self.__file_checks = hdfCheck(self)
 
+    def some_info(self):
+        return 'some info'
+
     @property
     def exp_descr(self):
         """

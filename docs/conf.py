0a2ddc2772fd2a2de1504f26e81bf6c0fce8038b
rocco8773 <eteverson@gmail.com>
2018-03-15 19:11:21 -0700

    conf.py: increased depth of readthedoc's navigation sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -101,7 +101,9 @@ html_theme = 'sphinx_rtd_theme'
 # theme further.  For a list of options available for each theme, see
 # the documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'navigation_depth': 8,  # depth of readthedocs sidebar
+}
 
 # Add any paths that contain custom static files (such as style sheets)
 # here, relative to this directory. They are copied after the builtin

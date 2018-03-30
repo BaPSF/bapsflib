a8301ece2b5574c2a2453a68031c2c747f3e129b
rocco8773 <eteverson@gmail.com>
2018-03-29 18:17:22 -0700

    map_controls/tests/common.py: added specified assertions for control devices that record data around the concept of command lists

--- bapsflib/lapdhdf/map_controls/tests/common.py
+++ bapsflib/lapdhdf/map_controls/tests/common.py
@@ -27,6 +27,15 @@ class ControlTestCase(ut.TestCase):
         self.assertTrue(hasattr(cmap, 'name'))
         self.assertTrue(hasattr(cmap, 'construct_dataset_name'))
         self.assertTrue(hasattr(cmap, 'unique_specifiers'))
+        if cmap.has_command_list:
+            self.assertTrue(hasattr(cmap, '_cl_re_patterns'))
+            self.assertTrue(hasattr(cmap,
+                                    '_default_probe_state_config'))
+            self.assertTrue(hasattr(cmap,
+                                    '_construct_probe_state_dict'))
+            self.assertTrue(hasattr(cmap, 'clparse'))
+            self.assertTrue(hasattr(cmap, 'reset_probe_state_config'))
+            self.assertTrue(hasattr(cmap, 'set_probe_state_config'))
 
         # test type and keys for map.info
         self.assertIsInstance(cmap.info, dict)
@@ -42,10 +51,35 @@ class ControlTestCase(ut.TestCase):
         self.assertIsInstance(cmap.configs, dict)
         for config in cmap.configs:
             self.assertIsInstance(cmap.configs[config], dict)
-            self.assertIn('dataset fields', cmap.configs[config])
-            self.assertIn('dset field to numpy field',
-                          cmap.configs[config])
-            self.assertIn('dset name', cmap.configs[config])
+            self.assertIn('dset paths', cmap.configs[config])
+            self.assertIn('shotnum', cmap.configs[config])
+            self.assertIn('probe state values', cmap.configs[config])
+            if cmap.has_command_list:
+                self.assertIn('command list', cmap.configs[config])
+
+            # 'shotnum' features
+            self.assertIsInstance(cmap.configs[config]['shotnum'], dict)
+            self.assertIn('dset paths', cmap.configs[config]['shotnum'])
+            self.assertIn('dset field', cmap.configs[config]['shotnum'])
+            self.assertIn('shape', cmap.configs[config]['shotnum'])
+            self.assertIn('dtype', cmap.configs[config]['shotnum'])
+
+            # 'probe state values' features
+            self.assertIsInstance(
+                cmap.configs[config]['probe state values'], dict)
+            for pstate_dict in \
+                    cmap.configs[config]['probe state values'].values():
+                self.assertIsInstance(pstate_dict, dict)
+                self.assertIn('dset paths', pstate_dict)
+                self.assertIn('dset field', pstate_dict)
+                self.assertIn('shape', pstate_dict)
+                self.assertIn('dtype', pstate_dict)
+
+                # for command list control devices
+                if cmap.has_command_list:
+                    self.assertIn('command list', pstate_dict)
+                    self.assertIn('cl str', pstate_dict)
+                    self.assertIn('re pattern', pstate_dict)
 
         # assert contype
         self.assertEqual(cmap.info['contype'], cmap.contype)

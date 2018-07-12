fde6134c3db95440310f2075ec60d6d01b057254
rocco8773 <eteverson@gmail.com>
2018-07-11 19:53:35 -0700

    added test method 'test_map_failures' to test conditions that cause unsuccessful mappings

--- bapsflib/lapdhdf/map_msi/tests/test_discharge.py
+++ bapsflib/lapdhdf/map_msi/tests/test_discharge.py
@@ -16,6 +16,7 @@ from .common import MSIDiagnosticTestCase
 
 from bapsflib.lapdhdf.tests import FauxHDFBuilder
 
+import numpy as np
 import unittest as ut
 
 
@@ -43,6 +44,102 @@ class TestDischarge(MSIDiagnosticTestCase):
     def test_map_basics(self):
         """Test all required basic map features."""
         self.assertMSIDiagMapBasics(self.map, self.dgroup)
+
+    def test_map_failures(self):
+        """Test conditions that result in unsuccessful mappings."""
+        # any failed build must throw a UserWarning
+        #
+        # not all required datasets are found                       ----
+        # - Datasets should be:
+        #   ~ 'Discharge summary'
+        #   ~ 'Cathode-anode voltage'
+        #   ~ 'Discharge current'
+        # - removed 'Discharge summary' from faux HDF file
+        #
+        del self.mod['Discharge summary']
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # 'Discharge summary' does NOT match expected format        ----
+        #
+        # 'Discharge summary' is missing a required field
+        data = self.mod['Discharge summary'][:]
+        fields = list(data.dtype.names)
+        fields.remove('Pulse length')
+        del self.mod['Discharge summary']
+        self.mod.create_dataset('Discharge summary', data=data[fields])
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # 'Discharge summary' is not a structured array
+        data = np.empty((2, 100), dtype=np.float64)
+        del self.mod['Discharge summary']
+        self.mod.create_dataset('Discharge summary', data=data)
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # 'Cathode-anode voltage' does NOT match expected format    ----
+        #
+        # dataset has fields
+        data = np.empty((2,), dtype=np.dtype([('field1', np.float64),
+                                              ('field2', np.float64)]))
+        del self.mod['Cathode-anode voltage']
+        self.mod.create_dataset('Cathode-anode voltage', data=data)
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # shape is not 2 dimensional
+        data = np.empty((2, 5, 100), dtype=np.float64)
+        del self.mod['Cathode-anode voltage']
+        self.mod.create_dataset('Cathode-anode voltage', data=data)
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # number of rows is NOT consistent with 'Discharge summary'
+        dtype = self.mod['Cathode-anode voltage'].dtype
+        shape = (self.mod['Cathode-anode voltage'].shape[0] + 1,
+                 self.mod['Cathode-anode voltage'].shape[1])
+        data = np.empty(shape, dtype=dtype)
+        del self.mod['Cathode-anode voltage']
+        self.mod.create_dataset('Cathode-anode voltage', data=data)
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # 'Discharge current' does NOT match expected format        ----
+        #
+        # dataset has fields
+        data = np.empty((2,), dtype=np.dtype([('field1', np.float64),
+                                              ('field2', np.float64)]))
+        del self.mod['Discharge current']
+        self.mod.create_dataset('Discharge current', data=data)
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # shape is not 2 dimensional
+        data = np.empty((2, 5, 100), dtype=np.float64)
+        del self.mod['Discharge current']
+        self.mod.create_dataset('Discharge current', data=data)
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # number of rows is NOT consistent with 'Discharge summary'
+        dtype = self.mod['Discharge current'].dtype
+        shape = (self.mod['Discharge current'].shape[0] + 1,
+                 self.mod['Discharge current'].shape[1])
+        data = np.empty(shape, dtype=dtype)
+        del self.mod['Discharge current']
+        self.mod.create_dataset('Discharge current', data=data)
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
 
 
 if __name__ == '__main__':

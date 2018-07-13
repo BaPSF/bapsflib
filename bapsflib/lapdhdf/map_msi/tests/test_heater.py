4735215d3210755759d0a02acd8299ff7f0ee2e2
rocco8773 <eteverson@gmail.com>
2018-07-13 15:25:33 -0700

    added test methods 'test_map_failures' and 'test_configs_general_items'

--- bapsflib/lapdhdf/map_msi/tests/test_heater.py
+++ bapsflib/lapdhdf/map_msi/tests/test_heater.py
@@ -16,12 +16,12 @@ from .common import MSIDiagnosticTestCase
 
 from bapsflib.lapdhdf.tests import FauxHDFBuilder
 
+import numpy as np
 import unittest as ut
 
 
 class TestHeater(MSIDiagnosticTestCase):
     """Test class for hdfMap_msi_heater"""
-    # TODO: ADD A WARN TEST IF BUILD UNSUCCESSFUL
 
     def setUp(self):
         self.f = FauxHDFBuilder(
@@ -43,6 +43,62 @@ class TestHeater(MSIDiagnosticTestCase):
     def test_map_basics(self):
         """Test all required basic map features."""
         self.assertMSIDiagMapBasics(self.map, self.dgroup)
+
+    def test_map_failures(self):
+        """Test conditions that result in unsuccessful mappings."""
+        # any failed build must throw a UserWarning
+        #
+        # not all required datasets are found                       ----
+        # - Datasets should be:
+        #   ~ 'Heater summary'
+        # - removed 'Heater summary' from faux HDF file
+        #
+        del self.mod['Heater summary']
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # 'Heater summary' does NOT match expected format           ----
+        #
+        # 'Heater summary' is missing a required field
+        data = self.mod['Heater summary'][:]
+        fields = list(data.dtype.names)
+        fields.remove('Heater current')
+        del self.mod['Heater summary']
+        self.mod.create_dataset('Heater summary', data=data[fields])
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # 'Discharge summary' is not a structured numpy array
+        data = np.empty((2, 100), dtype=np.float64)
+        del self.mod['Heater summary']
+        self.mod.create_dataset('Heater summary', data=data)
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+    def test_configs_general_items(self):
+        """
+        Test behavior for the general, polymorphic elements of the
+        `configs` mapping dictionary.
+        """
+        # ensure general items are present
+        self.assertIn('calib tag', self.map.configs)
+
+        # ensure general items have expected values
+        self.assertEqual(
+            [self.dgroup.attrs['Calibration tag']],
+            self.map.configs['calib tag'])
+
+        # check warning if an item is missing
+        # - a warning is thrown, but mapping continues
+        # - remove attribute 'Calibration tag'
+        del self.dgroup.attrs['Calibration tag']
+        with self.assertWarns(UserWarning):
+            self.assertIn('calib tag', self.map.configs)
+            self.assertEqual(self.map.configs['calib tag'], [])
+        self.mod.knobs.reset()
 
 
 if __name__ == '__main__':

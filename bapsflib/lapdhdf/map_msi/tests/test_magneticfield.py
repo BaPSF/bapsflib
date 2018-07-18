2912d7dd4ce86713fd5e9da7fa5f4b55ad7966ef
rocco8773 <eteverson@gmail.com>
2018-07-18 16:05:36 -0700

    added test methodes 'test_map_failures' and 'test_configs_general_items'

--- bapsflib/lapdhdf/map_msi/tests/test_magneticfield.py
+++ bapsflib/lapdhdf/map_msi/tests/test_magneticfield.py
@@ -16,6 +16,7 @@ from .common import MSIDiagnosticTestCase
 
 from bapsflib.lapdhdf.tests import FauxHDFBuilder
 
+import numpy as np
 import unittest as ut
 
 
@@ -43,6 +44,141 @@ class TestMagneticField(MSIDiagnosticTestCase):
     def test_map_basics(self):
         """Test all required basic map features."""
         self.assertMSIDiagMapBasics(self.map, self.dgroup)
+
+    def test_map_failures(self):
+        """Test conditions that result in unsuccessful mappings."""
+        # any failed build must throw a UserWarning
+        #
+        # not all required datasets are found                       ----
+        # - Datasets should be:
+        #   ~ 'Magnetic field summary'
+        #   ~ 'Magnetic field profile'
+        #   ~ 'Magnetic power supply currents'
+        # - removed 'Discharge summary' from faux HDF file
+        #
+        del self.mod['Magnetic field summary']
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # 'Magnetic field summary' does NOT match expected format   ----
+        #
+        # define dataset name
+        dset_name = 'Magnetic field summary'
+
+        # 'Magnetic field summary' is missing a required field
+        data = self.mod[dset_name][:]
+        fields = list(data.dtype.names)
+        fields.remove('Peak magnetic field')
+        del self.mod[dset_name]
+        self.mod.create_dataset(dset_name, data=data[fields])
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # 'Magnetic field summary' is not a structured numpy array
+        data = np.empty((2, 100), dtype=np.float64)
+        del self.mod[dset_name]
+        self.mod.create_dataset(dset_name, data=data)
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # 'Magnetic field profile' does NOT match expected format   ----
+        #
+        # define dataset name
+        dset_name = 'Magnetic field profile'
+
+        # dataset has fields
+        data = np.empty((2,), dtype=np.dtype([('field1', np.float64),
+                                              ('field2', np.float64)]))
+        del self.mod[dset_name]
+        self.mod.create_dataset(dset_name, data=data)
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # shape is not 2 dimensional
+        data = np.empty((2, 5, 100), dtype=np.float64)
+        del self.mod[dset_name]
+        self.mod.create_dataset(dset_name, data=data)
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # number of rows is NOT consistent with 'Magnetic field summary'
+        dtype = self.mod[dset_name].dtype
+        shape = (self.mod[dset_name].shape[0] + 1,
+                 self.mod[dset_name].shape[1])
+        data = np.empty(shape, dtype=dtype)
+        del self.mod[dset_name]
+        self.mod.create_dataset(dset_name, data=data)
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # 'Magnet power supply currents' does NOT match             ----
+        # expected format                                           ----
+        #
+        # define dataset name
+        dset_name = 'Magnet power supply currents'
+
+        # dataset has fields
+        data = np.empty((2,), dtype=np.dtype([('field1', np.float64),
+                                              ('field2', np.float64)]))
+        del self.mod[dset_name]
+        self.mod.create_dataset(dset_name, data=data)
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # shape is not 2 dimensional
+        data = np.empty((2, 5, 100), dtype=np.float64)
+        del self.mod[dset_name]
+        self.mod.create_dataset(dset_name, data=data)
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+        # number of rows is NOT consistent with 'Magnetic field summary'
+        dtype = self.mod[dset_name].dtype
+        shape = (self.mod[dset_name].shape[0] + 1,
+                 self.mod[dset_name].shape[1])
+        data = np.empty(shape, dtype=dtype)
+        del self.mod[dset_name]
+        self.mod.create_dataset(dset_name, data=data)
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.knobs.reset()
+
+    def test_configs_general_items(self):
+        """
+        Test behavior for the general, polymorphic elements of the
+        `configs` mapping dictionary.
+        """
+        # get map instance
+        _map = self.map
+
+        # ensure general items are present
+        self.assertIn('calib tag', _map.configs)
+        self.assertIn('z', _map.configs)
+
+        # ensure general items have expected values
+        self.assertEqual([self.dgroup.attrs['Calibration tag']],
+                         _map.configs['calib tag'])
+        self.assertTrue(np.array_equal(
+            self.dgroup.attrs['Profile z locations'],
+            _map.configs['z']))
+
+        # check warning if an item is missing
+        # - a warning is thrown, but mapping continues
+        # - remove attribute 'Profile z locations'
+        del self.dgroup.attrs['Profile z locations']
+        with self.assertWarns(UserWarning):
+            _map = self.map
+            self.assertIn('z', _map.configs)
+            self.assertEqual(_map.configs['z'], [])
+        self.mod.knobs.reset()
 
 
 if __name__ == '__main__':

c46d671818ae3453345cd056aaf9cd447470b164
rocco8773 <eteverson@gmail.com>
2018-07-18 20:19:10 -0700

    removed TODO

--- bapsflib/lapdhdf/map_msi/tests/test_magneticfield.py
+++ bapsflib/lapdhdf/map_msi/tests/test_magneticfield.py
@@ -22,7 +22,6 @@ import unittest as ut
 
 class TestMagneticField(MSIDiagnosticTestCase):
     """Test class for hdfMap_msi_magneticfield"""
-    # TODO: ADD A WARN TEST IF BUILD UNSUCCESSFUL
 
     def setUp(self):
         self.f = FauxHDFBuilder(

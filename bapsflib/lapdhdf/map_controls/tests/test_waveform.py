e96bffff2af1b7531cf075085deb8c9dd96507d1
rocco8773 <eteverson@gmail.com>
2018-07-26 17:28:50 -0700

    test_waveform.py: updated setup and teardown methods, added docstrings, improved readability of existing methods

--- bapsflib/lapdhdf/map_controls/tests/test_waveform.py
+++ bapsflib/lapdhdf/map_controls/tests/test_waveform.py
@@ -16,12 +16,12 @@ from .common import ControlTestCase
 
 from bapsflib.lapdhdf.tests import FauxHDFBuilder
 
+import numpy as np
 import unittest as ut
 
 
 class TestWaveform(ControlTestCase):
     """Test class for hdfMap_control_waveform"""
-    # N_CONFIGS = 1
 
     def setUp(self):
         self.f = FauxHDFBuilder(
@@ -33,11 +33,18 @@ class TestWaveform(ControlTestCase):
 
     @property
     def map(self):
-        return hdfMap_control_waveform(self.cgroup)
+        """Map object of control device"""
+        return self.map_control(self.cgroup)
 
     @property
     def cgroup(self):
+        """Control device group"""
         return self.f['Raw data + config/Waveform']
+
+    @staticmethod
+    def map_control(group):
+        """Mapping function"""
+        return hdfMap_control_waveform(group)
 
     def test_map_basics(self):
         self.assertControlMapBasics(self.map, self.cgroup)
@@ -49,6 +56,10 @@ class TestWaveform(ControlTestCase):
         self.assertEqual(self.map.info['contype'], 'waveform')
 
     def test_one_config(self):
+        """
+        Test mapping of the 'Waveform' group with only one
+        configuration.
+        """
         # reset to one config
         if self.mod.knobs.n_configs != 1:
             self.mod.knobs.n_configs = 1
@@ -57,6 +68,9 @@ class TestWaveform(ControlTestCase):
         self.assertWaveformDetails()
 
     def test_three_configs(self):
+        """
+        Test mapping of the 'Waveform' group with THREE configurations.
+        """
         # reset to 3 configs
         if self.mod.knobs.n_configs != 3:
             self.mod.knobs.n_configs = 3
@@ -65,41 +79,56 @@ class TestWaveform(ControlTestCase):
         self.assertWaveformDetails()
 
     def assertWaveformDetails(self):
+        """
+        Test details of a 'Waveform' mapping, i.e. the basic tests for
+        a control device plus the unique features for the 'Waveform'
+        group.
+        """
+        # define map instance
+        _map = self.map
+
+        # re-assert Mapping Basics
+        self.assertControlMapBasics(_map, self.cgroup)
+
         # test dataset names
-        self.assertEqual(self.map.dataset_names, ['Run time list'])
+        self.assertEqual(_map.dataset_names, ['Run time list'])
 
         # test construct_dataset_names
-        # TODO: how to test 'construct_dataset_names'
-
-        # re-test all sub-group names
-        self.assertSubgroupNames(self.map, self.cgroup)
-
-        # test attribute 'group'
-        self.assertEqual(self.map.group, self.cgroup)
+        self.assertEqual(_map.construct_dataset_name(), 'Run time list')
 
         # test for command list
-        self.assertTrue(self.map.has_command_list)
+        self.assertTrue(_map.has_command_list)
 
         # test attribute 'one_config_per_dataset'
         if self.mod.knobs.n_configs == 1:
-            self.assertTrue(self.map.one_config_per_dset)
+            self.assertTrue(_map.one_config_per_dset)
         else:
-            self.assertFalse(self.map.one_config_per_dset)
+            self.assertFalse(_map.one_config_per_dset)
 
         # test that 'configs' attribute is setup correctly
-        self.assertConfigs()
+        self.assertConfigsGeneralItems(_map)
 
-    def assertConfigs(self):
-        self.assertEqual(len(self.map.configs),
+    def assertConfigsGeneralItems(self, cmap):
+        """
+        Test structure of the general, polymorphic elements of the
+        `configs` mapping dictionary.
+        """
+        # only asserts 'Waveform' specific attributes
+        self.assertEqual(len(cmap.configs),
                          self.mod.knobs.n_configs)
 
-        for config in self.map.configs:
+        for cname, config in cmap.configs.items():
             # keys 'dataset fields' and 'dset to numpy field' tested in
             # assertControlMapBasic
             #
-            self.assertIn(config, self.mod.config_names)
-            self.assertIn('IP address', self.map.configs[config])
-            self.assertIn('device name', self.map.configs[config])
+            # - Note: 'command list' is not included since it is
+            #         covered by assertControlMapBasics()
+            #
+            self.assertIn(cname, self.mod.config_names)
+            self.assertIn('IP address', config)
+            self.assertIn('generator device', config)
+            self.assertIn('GPIB address', config)
+            self.assertIn('initial state', config)
 
 
 if __name__ == '__main__':

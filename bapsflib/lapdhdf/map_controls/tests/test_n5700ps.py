2616a856b1557f5e2204f7a340d3bb5f3cdf36bb
rocco8773 <eteverson@gmail.com>
2018-07-27 15:50:24 -0700

    test_n5700ps.py: updated setup and teardown methods, added docstrings, improved readability of existing methods

--- bapsflib/lapdhdf/map_controls/tests/test_n5700ps.py
+++ bapsflib/lapdhdf/map_controls/tests/test_n5700ps.py
@@ -32,11 +32,18 @@ class TestN5700PS(ControlTestCase):
 
     @property
     def map(self):
-        return hdfMap_control_n5700ps(self.cgroup)
+        """Map object of control device"""
+        return self.map_control(self.cgroup)
 
     @property
     def cgroup(self):
+        """Control device group"""
         return self.f['Raw data + config/N5700_PS']
+
+    @staticmethod
+    def map_control(group):
+        """Mapping function"""
+        return hdfMap_control_n5700ps(group)
 
     def test_map_basics(self):
         self.assertControlMapBasics(self.map, self.cgroup)
@@ -48,6 +55,10 @@ class TestN5700PS(ControlTestCase):
         self.assertEqual(self.map.info['contype'], 'power')
 
     def test_one_config(self):
+        """
+        Test mapping of the 'N5700_PS' group with only one
+        configuration.
+        """
         # reset to one config
         if self.mod.knobs.n_configs != 1:
             self.mod.knobs.n_configs = 1
@@ -56,6 +67,9 @@ class TestN5700PS(ControlTestCase):
         self.assertWaveformDetails()
 
     def test_three_configs(self):
+        """
+        Test mapping of the 'N5700_PS' group with THREE configurations.
+        """
         # reset to 3 configs
         if self.mod.knobs.n_configs != 3:
             self.mod.knobs.n_configs = 3
@@ -64,20 +78,25 @@ class TestN5700PS(ControlTestCase):
         self.assertWaveformDetails()
 
     def assertWaveformDetails(self):
+        """
+        Test details of a 'N5700_PS' mapping, i.e. the basic tests for
+        a control device plus the unique features for the 'N5700_PS'
+        group.
+        """
+        # define map instance
+        _map = self.map
+
+        # re-assert Mapping Basics
+        self.assertControlMapBasics(_map, self.cgroup)
+
         # test dataset names
-        self.assertEqual(self.map.dataset_names, ['Run time list'])
+        self.assertEqual(_map.dataset_names, ['Run time list'])
 
         # test construct_dataset_names
-        # TODO: how to test 'construct_dataset_names'
-
-        # re-test all sub-group names
-        self.assertSubgroupNames(self.map, self.cgroup)
-
-        # test attribute 'group'
-        self.assertEqual(self.map.group, self.cgroup)
+        self.assertEqual(_map.construct_dataset_name(), 'Run time list')
 
         # test for command list
-        self.assertTrue(self.map.has_command_list)
+        self.assertTrue(_map.has_command_list)
 
         # test attribute 'one_config_per_dataset'
         if self.mod.knobs.n_configs == 1:
@@ -86,20 +105,25 @@ class TestN5700PS(ControlTestCase):
             self.assertFalse(self.map.one_config_per_dset)
 
         # test that 'configs' attribute is setup correctly
-        self.assertConfigs()
+        self.assertConfigsGeneralItems(_map)
 
-    def assertConfigs(self):
-        self.assertEqual(len(self.map.configs),
+    def assertConfigsGeneralItems(self, cmap):
+        """
+        Test structure of the general, polymorphic elements of the
+        `configs` mapping dictionary.
+        """
+        # only asserts 'N5700_PS' specific attributes
+        self.assertEqual(len(cmap.configs),
                          self.mod.knobs.n_configs)
 
-        for config in self.map.configs:
-            # keys 'dataset fields' and 'dset to numpy field' tested in
-            # assertControlMapBasic
+        for cname, config in cmap.configs.items():
+            # Note: 'command list' is not included since it is
+            #         covered by assertControlMapBasics()
             #
-            self.assertIn(config, self.mod.config_names)
-            self.assertIn('IP address', self.map.configs[config])
-            self.assertIn('device model', self.map.configs[config])
-            self.assertIn('init command', self.map.configs[config])
+            self.assertIn(cname, self.mod.config_names)
+            self.assertIn('IP address', config)
+            self.assertIn('power supply device', config)
+            self.assertIn('initial state', config)
 
 
 if __name__ == '__main__':

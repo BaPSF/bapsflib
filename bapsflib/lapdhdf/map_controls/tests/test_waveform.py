a9f1629e64bb11a771b463c39e62207447292cec
rocco8773 <eteverson@gmail.com>
2018-07-26 17:29:47 -0700

    test_waveform.py: added test methods 'test_contype', 'test_not_h5py_group', 'test_map_failures', and 'test_misc'

--- bapsflib/lapdhdf/map_controls/tests/test_waveform.py
+++ bapsflib/lapdhdf/map_controls/tests/test_waveform.py
@@ -49,11 +49,60 @@ class TestWaveform(ControlTestCase):
     def test_map_basics(self):
         self.assertControlMapBasics(self.map, self.cgroup)
 
-    def test_info(self):
-        self.assertEqual(self.map.info['group name'], 'Waveform')
-        self.assertEqual(self.map.info['group path'],
-                         '/Raw data + config/Waveform')
+    def test_contype(self):
         self.assertEqual(self.map.info['contype'], 'waveform')
+
+    def test_not_h5py_group(self):
+        """Test error if object to map is not h5py.Group"""
+        with self.assertRaises(TypeError):
+            self.map_control(None)
+
+    def test_map_failures(self):
+        """Test conditions that result in unsuccessful mappings."""
+        # any failed build must throw a UserWarning
+        #
+        # make a default/clean 'Waveform' module
+        self.mod.knobs.reset()
+
+        # expected dataset does not exist
+        # - rename 'Run time list' dataset
+        self.mod.move('Run time list', 'Waveform data')
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod.move('Waveform data', 'Run time list')
+
+        # 'Waveform command list' attribute does not exist
+        #
+        config_name = self.mod.config_names[0]
+        cl = self.mod[config_name].attrs['Waveform command list']
+        self.mod[config_name].attrs['Wrong command list'] = cl
+        del self.mod[config_name].attrs['Waveform command list']
+        with self.assertWarns(UserWarning):
+            self.assertFalse(self.map.build_successful)
+        self.mod[config_name].attrs['Waveform command list'] = cl
+        del self.mod[config_name].attrs['Wrong command list']
+
+    def test_misc(self):
+        """Test Miscellaneous features."""
+        # make a default/clean 'Waveform' module
+        self.mod.knobs.reset()
+
+        # no RE match so _defaulte_state_values_dict() is used for
+        # 'command list'
+        #
+        config_name = self.mod.config_names[0]
+        cl = np.bytes_('AMP 10.0 \nAMP 15.0 \nAMP 20.0 \n')
+        self.mod[config_name].attrs['Waveform command list'] = cl
+        self.assertControlMapBasics(self.map, self.cgroup)
+
+        # check warning if a general item is missing from group
+        # - a warning is thrown, but mapping continues
+        # - remove attribute 'IP address'
+        config_name = self.mod.config_names[0]
+        del self.mod[config_name].attrs['IP address']
+        with self.assertWarns(UserWarning):
+            self.assertTrue(self.map.build_successful)
+        self.mod.knobs.reset()
 
     def test_one_config(self):
         """

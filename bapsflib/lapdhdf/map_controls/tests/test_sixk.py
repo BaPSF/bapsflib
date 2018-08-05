d618b2e185b383b1b7d5579ab31ab7f6a198e48a
rocco8773 <eteverson@gmail.com>
2018-08-05 16:23:53 -0700

    test_sixk.py: updated setup and teardown methods, added docstrings, improved readability of existing methods

--- bapsflib/lapdhdf/map_controls/tests/test_sixk.py
+++ bapsflib/lapdhdf/map_controls/tests/test_sixk.py
@@ -11,12 +11,16 @@
 # License: Standard 3-clause BSD; see "LICENSES/LICENSE.txt" for full
 #   license terms and contributor agreement.
 #
+import os
+
+import numpy as np
+import unittest as ut
+
 from ..sixk import hdfMap_control_6k
 from .common import ControlTestCase
 
+from unittest import mock
 from bapsflib.lapdhdf.tests import FauxHDFBuilder
-
-import unittest as ut
 
 
 class TestSixK(ControlTestCase):
@@ -32,19 +36,23 @@ class TestSixK(ControlTestCase):
 
     @property
     def map(self):
-        return hdfMap_control_6k(self.cgroup)
+        """Map object of control device"""
+        return self.map_control(self.cgroup)
 
     @property
     def cgroup(self):
+        """Control device group"""
         return self.f['Raw data + config/6K Compumotor']
+
+    @staticmethod
+    def map_control(group):
+        """Mapping function"""
+        return hdfMap_control_6k(group)
 
     def test_map_basics(self):
         self.assertControlMapBasics(self.map, self.cgroup)
 
-    def test_info(self):
-        self.assertEqual(self.map.info['group name'], '6K Compumotor')
-        self.assertEqual(self.map.info['group path'],
-                         '/Raw data + config/6K Compumotor')
+    def test_contype(self):
         self.assertEqual(self.map.info['contype'], 'motion')
 
     def test_one_config_one_ml(self):
@@ -76,43 +84,80 @@ class TestSixK(ControlTestCase):
         self.assertSixKDetails()
 
     def assertSixKDetails(self):
+        """
+        Test details of a '6K Compumotor' mapping, i.e. the basic tests
+        for a control device plus the unique features for the
+        '6K Compumotor' group.
+        """
+        # define map instance
+        _map = self.map
+
+        # re-assert Mapping Basics
+        self.assertControlMapBasics(_map, self.cgroup)
+
         # test dataset names
         # TODO: test dataset names
 
         # test construct_dataset_names
         # TODO: how to test 'construct_dataset_names'
 
-        # re-test all sub-group names
-        self.assertSubgroupNames(self.map, self.cgroup)
-
-        # test attribute 'group'
-        self.assertEqual(self.map.group, self.cgroup)
-
         # test for command list
-        self.assertFalse(self.map.has_command_list)
+        self.assertFalse(_map.has_command_list)
 
         # test attribute 'one_config_per_dataset'
-        self.assertTrue(self.map.one_config_per_dset)
+        self.assertTrue(_map.one_config_per_dset)
 
         # test that 'configs' attribute is setup correctly
-        self.assertConfigs()
+        self.assertConfigsGeneralItems(_map)
 
-    def assertConfigs(self):
-        self.assertEqual(len(self.map.configs),
+    def assertConfigsGeneralItems(self, cmap):
+        """
+        Test structure of the general, polymorphic elements of the
+        `configs` mapping dictionary.
+        """
+        self.assertEqual(len(cmap.configs),
                          self.mod.knobs.n_configs)
 
-        for config in self.map.configs:
-            # keys 'dataset fields' and 'dset to numpy field' tested in
-            # assertControlMapBasic
+        for cname, config in cmap.configs.items():
+            # look for '6K Compumotor' specific keys
             #
-            self.assertIn(config, self.mod.config_names)
-            self.assertIn('probe name', self.map.configs[config])
-            self.assertIn('port', self.map.configs[config])
-            self.assertIn('receptacle', self.map.configs[config])
-            self.assertIn('motion lists', self.map.configs[config])
+            self.assertIn(cname, self.mod.config_names)
+            self.assertIn('receptacle', config)
+            self.assertIn('probe', config)
+            self.assertIn('motion lists', config)
 
-            self.assertEqual(config,
-                             self.map.configs[config]['receptacle'])
+            # inspect 'receptacle' item
+            self.assertTrue(
+                np.issubdtype(type(config['receptacle']), np.integer))
+            self.assertTrue(config['receptacle'] > 0)
+            self.assertEqual(cname, config['receptacle'])
+
+            # inspect 'probe' item
+            self.assertIsInstance(config['probe'], dict)
+            self.assertProbeConfigDict(config['probe'])
+
+            # inspect 'motion lists' item
+            self.assertIsInstance(config['motion lists'], dict)
+            for val in config['motion lists'].values():
+                self.assertMLConfigDict(val)
+
+    def assertProbeConfigDict(self, config: dict):
+        """Test existence of keys for a 'probe' item config"""
+        pkeys = ['probe name', 'group name', 'group path',
+                 'receptacle', 'calib', 'level sy (cm)',
+                 'port', 'probe channels', 'probe type', 'unnamed',
+                 'sx at end (cm)', 'z']
+        for key in pkeys:
+            self.assertIn(key, config)
+
+    def assertMLConfigDict(self, config: dict):
+        """Test existence of keys for a 'motion list' item config"""
+        self.assertIsInstance(config, dict)
+        mkeys = ['group name', 'group path', 'created date',
+                 'data motion count', 'motion count',
+                 'delta', 'center', 'npoints']
+        for key in mkeys:
+            self.assertIn(key, config)
 
 
 if __name__ == '__main__':

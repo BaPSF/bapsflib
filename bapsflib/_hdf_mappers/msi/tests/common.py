f531b103b750a48d9f484098103f72c25e173ec8
rocco8773 <eteverson@gmail.com>
2018-08-31 18:58:00 -0700

    refactor 'dmap' to '_map' and 'dgroup' to '_group'

--- bapsflib/_hdf_mappers/msi/tests/common.py
+++ bapsflib/_hdf_mappers/msi/tests/common.py
@@ -18,126 +18,126 @@ import unittest as ut
 
 class MSIDiagnosticTestCase(ut.TestCase):
 
-    def assertMSIDiagMapBasics(self, dmap, dgroup):
+    def assertMSIDiagMapBasics(self, _map, _group):
         # check mapping instance
-        self.assertIsInstance(dmap, hdfMap_msi_template)
+        self.assertIsInstance(_map, hdfMap_msi_template)
 
         # assert attribute existence
-        self.assertTrue(hasattr(dmap, 'info'))
-        self.assertTrue(hasattr(dmap, 'device_name'))
-        self.assertTrue(hasattr(dmap, 'configs'))
-        self.assertTrue(hasattr(dmap, 'group'))
-        self.assertTrue(hasattr(dmap, 'build_successful'))
+        self.assertTrue(hasattr(_map, 'info'))
+        self.assertTrue(hasattr(_map, 'device_name'))
+        self.assertTrue(hasattr(_map, 'configs'))
+        self.assertTrue(hasattr(_map, 'group'))
+        self.assertTrue(hasattr(_map, 'build_successful'))
 
         # ---- test map.info                                        ----
         # test 'info' type
-        self.assertIsInstance(dmap.info, dict)
+        self.assertIsInstance(_map.info, dict)
 
         # check 'info' keys
-        self.assertIn('group name', dmap.info)
-        self.assertIn('group path', dmap.info)
+        self.assertIn('group name', _map.info)
+        self.assertIn('group path', _map.info)
 
         # check 'info' values
-        self.assertEqual(dmap.info['group name'],
-                         os.path.basename(dgroup.name))
-        self.assertEqual(dmap.info['group path'], dgroup.name)
+        self.assertEqual(_map.info['group name'],
+                         os.path.basename(_group.name))
+        self.assertEqual(_map.info['group path'], _group.name)
 
         # ---- test map.device_name                             ----
-        self.assertEqual(dmap.device_name, dmap.info['group name'])
+        self.assertEqual(_map.device_name, _map.info['group name'])
 
         # ---- test map.group                                       ----
         # check 'group' type
-        self.assertIsInstance(dmap.group, h5py.Group)
+        self.assertIsInstance(_map.group, h5py.Group)
 
         # ---- test map.build_successful                            ----
         # - all assertions below will only pass if build was successful
-        self.assertIsInstance(dmap.build_successful, bool)
-        self.assertTrue(dmap.build_successful)
+        self.assertIsInstance(_map.build_successful, bool)
+        self.assertTrue(_map.build_successful)
 
         # ---- test map.configs                                     ----
         # - must be a dict
         # TODO: write once format is pinned down
-        self.assertIsInstance(dmap.configs, dict)
+        self.assertIsInstance(_map.configs, dict)
 
         # look for required keys
-        self.assertIn('shape', dmap.configs)
-        self.assertIn('shotnum', dmap.configs)
-        self.assertIn('signals', dmap.configs)
-        self.assertIn('meta', dmap.configs)
+        self.assertIn('shape', _map.configs)
+        self.assertIn('shotnum', _map.configs)
+        self.assertIn('signals', _map.configs)
+        self.assertIn('meta', _map.configs)
 
         # inspect non-required keys
         # TODO: IS INSPECTION OF NON-REQUIRED KEYS NEEDED
 
         # examine 'shotnum' key
-        self.assertIsInstance(dmap.configs['shotnum'], dict)
-        self.assertIn('dset paths', dmap.configs['shotnum'])
-        self.assertIn('dset field', dmap.configs['shotnum'])
-        self.assertIn('shape', dmap.configs['shotnum'])
-        self.assertIn('dtype', dmap.configs['shotnum'])
+        self.assertIsInstance(_map.configs['shotnum'], dict)
+        self.assertIn('dset paths', _map.configs['shotnum'])
+        self.assertIn('dset field', _map.configs['shotnum'])
+        self.assertIn('shape', _map.configs['shotnum'])
+        self.assertIn('dtype', _map.configs['shotnum'])
         self.assertIsInstance(
-            dmap.configs['shotnum']['dset paths'], list)
+            _map.configs['shotnum']['dset paths'], list)
         self.assertTrue(
             all([isinstance(path, str)
-                 for path in dmap.configs['shotnum']['dset paths']]))
+                 for path in _map.configs['shotnum']['dset paths']]))
         self.assertIsInstance(
-            dmap.configs['shotnum']['shape'], list)
+            _map.configs['shotnum']['shape'], list)
         self.assertTrue(
             all([isinstance(shape, tuple)
-                 for shape in dmap.configs['shotnum']['shape']]))
+                 for shape in _map.configs['shotnum']['shape']]))
 
         # examine 'signals' key
-        self.assertIsInstance(dmap.configs['signals'], dict)
-        for field in dmap.configs['signals']:
-            self.assertIsInstance(dmap.configs['signals'][field], dict)
-            self.assertIn('dset paths', dmap.configs['signals'][field])
-            self.assertIn('dset field', dmap.configs['signals'][field])
-            self.assertIn('shape', dmap.configs['signals'][field])
-            self.assertIn('dtype', dmap.configs['signals'][field])
+        self.assertIsInstance(_map.configs['signals'], dict)
+        for field in _map.configs['signals']:
+            self.assertIsInstance(_map.configs['signals'][field], dict)
+            self.assertIn('dset paths', _map.configs['signals'][field])
+            self.assertIn('dset field', _map.configs['signals'][field])
+            self.assertIn('shape', _map.configs['signals'][field])
+            self.assertIn('dtype', _map.configs['signals'][field])
 
             # 'dset paths'
             self.assertIsInstance(
-                dmap.configs['signals'][field]['dset paths'], list)
+                _map.configs['signals'][field]['dset paths'], list)
             self.assertTrue(
                 all([isinstance(path, str)
                      for path in
-                     dmap.configs['signals'][field]['dset paths']]))
+                     _map.configs['signals'][field]['dset paths']]))
 
             # 'shape'
             self.assertIsInstance(
-                dmap.configs['signals'][field]['shape'], list)
+                _map.configs['signals'][field]['shape'], list)
             self.assertTrue(all(
                 [isinstance(shape, tuple)
-                 for shape in dmap.configs['signals'][field]['shape']]
+                 for shape in _map.configs['signals'][field]['shape']]
             ))
 
         # examine 'meta' key
-        self.assertIsInstance(dmap.configs['meta'], dict)
-        self.assertIn('shape', dmap.configs['meta'])
-        for field in dmap.configs['meta']:
+        self.assertIsInstance(_map.configs['meta'], dict)
+        self.assertIn('shape', _map.configs['meta'])
+        for field in _map.configs['meta']:
             if field == 'shape':
                 # key 'shape' will not be a numpy field, skip below
                 # assertions
                 continue
 
             # examine each sub-field in 'meta'
-            self.assertIsInstance(dmap.configs['meta'][field], dict)
-            self.assertIn('dset paths', dmap.configs['meta'][field])
-            self.assertIn('dset field', dmap.configs['meta'][field])
-            self.assertIn('shape', dmap.configs['meta'][field])
-            self.assertIn('dtype', dmap.configs['meta'][field])
+            self.assertIsInstance(_map.configs['meta'][field], dict)
+            self.assertIn('dset paths', _map.configs['meta'][field])
+            self.assertIn('dset field', _map.configs['meta'][field])
+            self.assertIn('shape', _map.configs['meta'][field])
+            self.assertIn('dtype', _map.configs['meta'][field])
 
             # 'dset paths'
             self.assertIsInstance(
-                dmap.configs['meta'][field]['dset paths'], list)
+                _map.configs['meta'][field]['dset paths'], list)
             self.assertTrue(
                 all([isinstance(path, str)
                      for path in
-                     dmap.configs['meta'][field]['dset paths']]))
+                     _map.configs['meta'][field]['dset paths']]))
 
             # 'shape'
             self.assertIsInstance(
-                dmap.configs['meta'][field]['shape'], list)
+                _map.configs['meta'][field]['shape'], list)
             self.assertTrue(all(
                 [isinstance(shape, tuple)
-                 for shape in dmap.configs['meta'][field]['shape']]
+                 for shape in _map.configs['meta'][field]['shape']]
             ))

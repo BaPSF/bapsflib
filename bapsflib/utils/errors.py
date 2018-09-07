f54b1ba46db87c7e1c7ebfb2407f8aabe0a3c2e9
rocco8773 <eteverson@gmail.com>
2018-09-07 11:15:44 -0700

    initial commit for package `utils` and submodule `errors.py`

---
+++ bapsflib/utils/errors.py
@@ -0,0 +1,16 @@
+# This file is part of the bapsflib package, a Python toolkit for the
+# BaPSF group at UCLA.
+#
+# http://plasma.physics.ucla.edu/
+#
+# Copyright 2017-2018 Erik T. Everson and contributors
+#
+# License: Standard 3-clause BSD; see "LICENSES/LICENSE.txt" for full
+#   license terms and contributor agreement.
+#
+
+
+class HDFMappingError(Exception):
+    """Exception for failed HDF5 mappings"""
+    def __init__(self, device_name: str, why=''):
+        super().__init__("'" + device_name + "' mapping failed: " + why)

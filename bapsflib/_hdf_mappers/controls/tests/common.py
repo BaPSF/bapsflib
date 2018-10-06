f6c2e71e5429089eacc19789dc8ff36e65335e53
rocco8773 <eteverson@gmail.com>
2018-10-06 15:49:08 -0700

    fix misspellings in docstrings

--- bapsflib/_hdf_mappers/controls/tests/common.py
+++ bapsflib/_hdf_mappers/controls/tests/common.py
@@ -22,7 +22,7 @@ from ..templates import (HDFMapControlTemplate,
 
 
 def method_overridden(cls, obj, method: str) -> bool:
-    """check is obj's class over-road base class method"""
+    """check if obj's class over-road base class method"""
     obj_method = method in obj.__class__.__dict__.keys()
     base_method = method in cls.__dict__.keys()
     print(obj_method, base_method)

91a23d7c7a90afc322815109524c9a38fc5274fe
rocco8773 <eteverson@gmail.com>
2018-10-09 19:48:31 -0700

    sort lists for equal assertion

--- bapsflib/_hdf_mappers/digitizers/tests/test_sis3301.py
+++ bapsflib/_hdf_mappers/digitizers/tests/test_sis3301.py
@@ -780,8 +780,8 @@ class TestSIS3301(DigitizerTestCase):
         _map = self.map
         self.assertDigitizerMapBasics(_map, self.dgroup)
         self.assertEqual(_map.active_configs, [config_name])
-        self.assertEqual(set(list(_map.configs)),
-                         set(self.mod.config_names))
+        self.assertEqual(sorted(list(_map.configs)),
+                         sorted(self.mod.config_names))
         self.assertConnectionsEqual(_map, tuple(my_bcs),
                                     adc, config_name)
 
@@ -805,7 +805,8 @@ class TestSIS3301(DigitizerTestCase):
         _map = self.map
         self.assertDigitizerMapBasics(_map, self.dgroup)
         self.assertEqual(_map.active_configs, list(config_names))
-        self.assertEqual(list(_map.configs), self.mod.config_names)
+        self.assertEqual(sorted(list(_map.configs)),
+                         sorted(self.mod.config_names))
         for config_name in config_names:
             self.assertConnectionsEqual(_map, tuple(my_bcs),
                                         adc, config_name)

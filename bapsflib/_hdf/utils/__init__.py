bbd2ffdd25035499e0b09da79b6a1e3e79f6c805
rocco8773 <eteverson@gmail.com>
2018-10-16 18:17:14 -0700

    create sub-package `bapsflib._hdf.utils` (for HDF5 file access and interface classes)

---
+++ bapsflib/_hdf/utils/__init__.py
@@ -0,0 +1,14 @@
+# This file is part of the bapsflib package, a Python toolkit for the
+# BaPSF group at UCLA.
+#
+# http://plasma.physics.ucla.edu/
+#
+# Copyright 2017-2018 Erik T. Everson and contributors
+#
+# License: Standard 3-clause BSD; see "LICENSES/LICENSE.txt" for full
+#   license terms and contributor agreement.
+#
+"""
+This package contains an assortment of utility classes used to
+access and interface with the HDF5 files generated at BaPSF.
+"""

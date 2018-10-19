59a0f8be2661825f6ec68eb467a13ca929d381d0
rocco8773 <eteverson@gmail.com>
2018-10-18 17:55:21 -0700

    redo entire test case to fit with updated `HDFMap` class; improve setup and teardown; def `assertHDFMapBasics`

--- bapsflib/_hdf/maps/tests/test_hdfmap.py
+++ bapsflib/_hdf/maps/tests/test_hdfmap.py
@@ -14,126 +14,348 @@
 import unittest as ut
 
 from .fauxhdfbuilder import FauxHDFBuilder
+from ..controls import HDFMapControls
+from ..controls.templates import HDFMapControlTemplate
+from ..digitizers import HDFMapDigitizers
+from ..digitizers.templates import HDFMapDigiTemplate
 from ..hdfmap import HDFMap
+from ..msi import HDFMapMSI
+from ..msi.templates import HDFMapMSITemplate
 
 
 class TestHDFMap(ut.TestCase):
     """
     Test Case for :class:`~bapsflib._hdf.maps.hdfmap.HDFMap`
     """
+
+    f = NotImplemented  # type: FauxHDFBuilder
+    MAP_CLASS = HDFMap
+
+    @classmethod
+    def setUpClass(cls):
+        # create hDF5 file
+        super().setUpClass()
+        cls.f = FauxHDFBuilder()
+
     def setUp(self):
-        self.f = FauxHDFBuilder()
+        super().setUp()
 
     def tearDown(self):
-        self.f.cleanup()
+        super().tearDown()
+        self.f.remove_all_modules()
+
+    @classmethod
+    def tearDownClass(cls):
+        # cleanup and close HDF5 file
+        super().tearDownClass()
+        cls.f.cleanup()
 
     @property
     def map(self):
-        return HDFMap(self.f)
+        return self.map_file(self.f, msi_path='MSI',
+                             digitizer_path='Raw data + config',
+                             control_path='Raw data + config')
 
-    def test_attribute_existence(self):
-        """Test existence of required attributes."""
-        # informational attributes
-        self.assertTrue(hasattr(self.map, 'msi'))
-        self.assertTrue(hasattr(self.map, 'digitizers'))
-        self.assertTrue(hasattr(self.map, 'main_digitizer'))
-        self.assertTrue(hasattr(self.map, 'controls'))
-        self.assertTrue(hasattr(self.map, 'unknowns'))
-        self.assertTrue(hasattr(self.map, 'hdf_version'))
+    def map_file(self, file, msi_path, digitizer_path, control_path):
+        return self.MAP_CLASS(file, msi_path=msi_path,
+                              digitizer_path=digitizer_path,
+                              control_path=control_path)
 
-        # has indicators
-        self.assertTrue(hasattr(self.map, 'has_msi_group'))
-        self.assertTrue(hasattr(self.map, 'has_data_group'))
-        self.assertTrue(hasattr(self.map, 'has_data_run_sequence'))
-        self.assertTrue(hasattr(self.map, 'has_msi_diagnostics'))
-        self.assertTrue(hasattr(self.map, 'has_digitizers'))
-        self.assertTrue(hasattr(self.map, 'has_controls'))
-        self.assertTrue(hasattr(self.map, 'has_unknowns'))
-        self.assertTrue(hasattr(self.map, 'is_lapd_hdf'))
+    def test_control_mapping_attachment(self):
+        """Test attachment of mapped control devices."""
+        #   1. specified control path does NOT exist
+        #   2. control path VALID, but NO controls
+        #   3. control path VALID, mixture of mappable and non-mappable
+        #      controls
+        #
+        # specified control path does NOT exist                      (1)
+        with self.assertWarns(UserWarning):
+            _map = self.map_file(self.f, msi_path='MSI',
+                                 digitizer_path='Raw data + config',
+                                 control_path='Not a control path')
+            self.assertHDFMapBasics(_map, self.f)
+            self.assertEqual(_map.controls, {})
 
-    def test_hdf_version(self):
-        """
-        Test LaPD HDF5 software version attribute is properly read.
-        """
-        self.assertEqual(self.map.hdf_version, '0.0.0')
+        # control path VALID, but NO controls                        (2)
+        _map = self.map_file(self.f, msi_path='MSI',
+                             digitizer_path='Raw data + config',
+                             control_path='Raw data + config')
+        self.assertHDFMapBasics(_map, self.f)
+        self.assertEqual(_map.controls, {})
+        self.assertIsInstance(_map.controls, HDFMapControls)
 
-    def test_is_lapd_hdf(self):
-        """Test attriubte is_lapd_hdf"""
-        self.assertTrue(self.map.is_lapd_hdf)
-
-    def test_base_hdf(self):
-        """
-        Test map structure for file with no diagnostics, digitizers,
-        or control devices
-        """
-        # existence of root groups
-        self.assertTrue(self.map.has_msi_group)
-        self.assertTrue(self.map.has_data_group)
-
-        # ensure HDF is empty
-        self.assertFalse(self.map.has_data_run_sequence)
-        self.assertFalse(self.map.has_msi_diagnostics)
-        self.assertFalse(self.map.has_digitizers)
-        self.assertFalse(self.map.has_controls)
-        self.assertFalse(self.map.has_unknowns)
-
-        # ensure format of informational attributes
-        self.assertIsInstance(self.map.msi, dict)
-        self.assertEqual(len(self.map.msi), 0)
-        self.assertIsInstance(self.map.digitizers, dict)
-        self.assertEqual(len(self.map.digitizers), 0)
-        self.assertIsInstance(self.map.controls, dict)
-        self.assertEqual(len(self.map.controls), 0)
-        self.assertIsInstance(self.map.unknowns, list)
-        self.assertEqual(len(self.map.unknowns), 0)
-        self.assertIs(self.map.main_digitizer, None)
-
-    def test_hdf_one_control(self):
-        """
-        Test map structure for file with NO diagnostics, NO digitizers,
-        and ONE control device ('Waveform').
-        """
-        # add waveform control
-        if len(self.f.modules) >= 1:
-            self.f.remove_all_modules()
+        # control path VALID, mixture of mappable and                (3)
+        # non-mappable controls
         self.f.add_module('Waveform')
+        self.f.create_group('Raw data + config/Not control or digi')
+        _map = self.map_file(self.f, msi_path='MSI',
+                             digitizer_path='Raw data + config',
+                             control_path='Raw data + config')
+        self.assertHDFMapBasics(_map, self.f)
+        self.assertIsInstance(_map.controls, HDFMapControls)
+        self.assertEqual(len(_map.controls), 1)
+        self.assertIn('Waveform', _map.controls)
+        self.assertEqual(_map.unknowns,
+                         ['/Raw data + config/Not control or digi'])
+        del self.f['Raw data + config/Not control or digi']
 
-        # existence of root groups
-        self.assertTrue(self.map.has_msi_group)
-        self.assertTrue(self.map.has_data_group)
+    def test_device_all_in_one_group(self):
+        """
+        Test the case where all devices (Controls, Digitizers, and
+        MSI diagnostics) share the same group.
+        """
+        # add one of each device
+        self.f.add_module('Waveform')
+        self.f.add_module('SIS 3301')
+        self.f.add_module('Discharge')
+        self.f.move('/MSI/Discharge', '/Raw data + config/Discharge')
+        _map = self.map_file(self.f, msi_path='Raw data + config',
+                             digitizer_path='Raw data + config',
+                             control_path='Raw data + config')
+        self.assertHDFMapBasics(_map, self.f)
 
-        # ensure HDF only has controls
-        self.assertFalse(self.map.has_data_run_sequence)
-        self.assertFalse(self.map.has_msi_diagnostics)
-        self.assertFalse(self.map.has_digitizers)
-        self.assertTrue(self.map.has_controls)
-        self.assertFalse(self.map.has_unknowns)
+        self.assertIsInstance(_map.controls, HDFMapControls)
+        self.assertEqual(len(_map.controls), 1)
+        self.assertIn('Waveform', _map.controls)
 
-        # ensure format of informational attributes
-        self.assertIsInstance(self.map.msi, dict)
-        self.assertEqual(len(self.map.msi), 0)
-        self.assertIsInstance(self.map.digitizers, dict)
-        self.assertEqual(len(self.map.digitizers), 0)
-        self.assertIsInstance(self.map.controls, dict)
-        self.assertEqual(len(self.map.controls), 1)
-        self.assertIsInstance(self.map.unknowns, list)
-        self.assertEqual(len(self.map.unknowns), 0)
-        self.assertIs(self.map.main_digitizer, None)
+        self.assertIsInstance(_map.digitizers, HDFMapDigitizers)
+        self.assertEqual(len(_map.digitizers), 1)
+        self.assertIn('SIS 3301', _map.digitizers)
 
-        # remove waveform control
-        self.f.remove_module('Waveform')
+        self.assertIsInstance(_map.msi, HDFMapMSI)
+        self.assertEqual(len(_map.msi), 1)
+        self.assertIn('Discharge', _map.msi)
 
-    def test_hdf_one_digitizer(self):
-        # TODO: write tests for mapping one digitizer
-        pass
+        self.assertEqual(_map.unknowns, ['/MSI'])
 
-    def test_hdf_one_diagnostic(self):
-        # TODO: write tests for mapping one msi diagnostic
-        pass
+        # add an unknown device
+        self.f.create_group('Raw data + config/Unknown')
+        _map = self.map_file(self.f, msi_path='Raw data + config',
+                             digitizer_path='Raw data + config',
+                             control_path='Raw data + config')
+        self.assertHDFMapBasics(_map, self.f)
 
-    def test_hdf_one_unknown(self):
-        # TODO: write tests for mapping of unknown modules (Groups)
-        pass
+        self.assertIsInstance(_map.controls, HDFMapControls)
+        self.assertEqual(len(_map.controls), 1)
+        self.assertIn('Waveform', _map.controls)
+
+        self.assertIsInstance(_map.digitizers, HDFMapDigitizers)
+        self.assertEqual(len(_map.digitizers), 1)
+        self.assertIn('SIS 3301', _map.digitizers)
+
+        self.assertIsInstance(_map.msi, HDFMapMSI)
+        self.assertEqual(len(_map.msi), 1)
+        self.assertIn('Discharge', _map.msi)
+
+        self.assertEqual(sorted(_map.unknowns),
+                         sorted(['/MSI', '/Raw data + config/Unknown']))
+
+        del self.f['Raw data + config/Unknown']
+
+    def test_digitizer_mapping_attachment(self):
+        """Test attachment of mapped digitizers."""
+        #   1. specified digitizer path does NOT exist
+        #   2. digitizer path VALID, but NO digitizers
+        #   3. digitizer path VALID, mixture of mappable and
+        #      non-mappable digitizers
+        #
+        # specified digitizer path does NOT exist                    (1)
+        with self.assertWarns(UserWarning):
+            _map = self.map_file(self.f, msi_path='MSI',
+                                 digitizer_path='Not a digi path',
+                                 control_path='Raw data + config')
+            self.assertHDFMapBasics(_map, self.f)
+            self.assertEqual(_map.digitizers, {})
+
+        # digitizer path VALID, but NO digitizers                    (2)
+        _map = self.map_file(self.f, msi_path='MSI',
+                             digitizer_path='Raw data + config',
+                             control_path='Raw data + config')
+        self.assertHDFMapBasics(_map, self.f)
+        self.assertEqual(_map.digitizers, {})
+        self.assertIsInstance(_map.digitizers, HDFMapDigitizers)
+
+        # digitizer path VALID, mixture of mappable and              (3)
+        # non-mappable digitizers
+        self.f.add_module('SIS 3301')
+        self.f.create_group('Raw data + config/Not control or digi')
+        _map = self.map_file(self.f, msi_path='MSI',
+                             digitizer_path='Raw data + config',
+                             control_path='Raw data + config')
+        self.assertHDFMapBasics(_map, self.f)
+        self.assertIsInstance(_map.digitizers, HDFMapDigitizers)
+        self.assertEqual(len(_map.digitizers), 1)
+        self.assertIn('SIS 3301', _map.digitizers)
+        self.assertEqual(_map.unknowns,
+                         ['/Raw data + config/Not control or digi'])
+        del self.f['Raw data + config/Not control or digi']
+
+    def test_empty_file(self):
+        """Test a file that is empty of any devices."""
+        # all devices are in root
+        _map = self.map_file(self.f, msi_path='',
+                             digitizer_path='', control_path='')
+        self.assertHDFMapBasics(_map, self.f)
+        self.assertEqual(_map.controls, {})
+        self.assertEqual(_map.digitizers, {})
+        self.assertEqual(_map.msi, {})
+        self.assertEqual(sorted(_map.unknowns),
+                         sorted(['/MSI', '/Raw data + config']))
+        self.assertIs(_map.main_digitizer, None)
+
+        # only controls are in root
+        _map = self.map_file(self.f, msi_path='MSI',
+                             digitizer_path='Raw data + config',
+                             control_path='/')
+        self.assertHDFMapBasics(_map, self.f)
+        self.assertEqual(_map.controls, {})
+        self.assertEqual(_map.digitizers, {})
+        self.assertEqual(_map.msi, {})
+        self.assertEqual(_map.unknowns, [])
+        self.assertIs(_map.main_digitizer, None)
+
+    def test_main_digitizer(self):
+        """Test identification of the "main" digitizer"""
+        # 1. there are no mapped digitizers
+        # 2. there is one mapped digitizer
+        # 3. the are MULTIPLE mapped digitizers
+        #
+        # there are no mapped digitizers                             (1)
+        _map = self.map_file(self.f, msi_path='MSI',
+                             digitizer_path='Raw data + config',
+                             control_path='Raw data + config')
+        self.assertHDFMapBasics(_map, self.f)
+        self.assertEqual(_map.digitizers, {})
+        self.assertIs(_map.main_digitizer, None)
+
+        # there is ONE mapped digitizer                              (2)
+        self.f.add_module('SIS 3301')
+        _map = self.map_file(self.f, msi_path='MSI',
+                             digitizer_path='Raw data + config',
+                             control_path='Raw data + config')
+        self.assertHDFMapBasics(_map, self.f)
+        self.assertEqual(_map.main_digitizer,
+                         _map.digitizers['SIS 3301'])
+
+        # there are MULTIPLE mapped digitizerS                       (3)
+        self.f.add_module('SIS crate')
+        _map = self.map_file(self.f, msi_path='MSI',
+                             digitizer_path='Raw data + config',
+                             control_path='Raw data + config')
+        self.assertHDFMapBasics(_map, self.f)
+        self.assertEqual(len(_map.digitizers), 2)
+        self.assertIn('SIS 3301', _map.digitizers)
+        self.assertIn('SIS crate', _map.digitizers)
+        self.assertEqual(_map.main_digitizer,
+                         _map.digitizers['SIS 3301'])
+
+    def test_msi_mapping_attachment(self):
+        """Test attachment of mapped MSI diagnostics."""
+        #   1. specified MSI path does NOT exist
+        #   2. MSI path VALID, but NO MSI diagnostics
+        #   3. MSI path VALID, mixture of mappable and
+        #      non-mappable MSI diagnostics
+        #
+        # specified MSI path does NOT exist                          (1)
+        with self.assertWarns(UserWarning):
+            _map = self.map_file(self.f, msi_path='Not a MSI path',
+                                 digitizer_path='Raw data + config',
+                                 control_path='Raw data + config')
+            self.assertHDFMapBasics(_map, self.f)
+            self.assertEqual(_map.msi, {})
+
+        # MSI path VALID, but NO MSI diagnostics                     (2)
+        _map = self.map_file(self.f, msi_path='MSI',
+                             digitizer_path='Raw data + config',
+                             control_path='Raw data + config')
+        self.assertHDFMapBasics(_map, self.f)
+        self.assertEqual(_map.msi, {})
+        self.assertIsInstance(_map.msi, HDFMapMSI)
+
+        # MSI path VALID, mixture of mappable and                    (3)
+        # non-mappable MSI diagnostics
+        self.f.add_module('Discharge')
+        self.f.add_module('Gas pressure')
+        self.f.create_group('MSI/Not a MSI')
+        _map = self.map_file(self.f, msi_path='MSI',
+                             digitizer_path='Raw data + config',
+                             control_path='Raw data + config')
+        self.assertHDFMapBasics(_map, self.f)
+        self.assertIsInstance(_map.msi, HDFMapMSI)
+        self.assertEqual(len(_map.msi), 2)
+        self.assertIn('Discharge', _map.msi)
+        self.assertIn('Gas pressure', _map.msi)
+        self.assertEqual(_map.unknowns, ['/MSI/Not a MSI'])
+        del self.f['MSI/Not a MSI']
+
+    def test_not_h5py_group(self):
+        """Test error if object to map is not h5py.Group"""
+        with self.assertRaises(TypeError):
+            self.map_file(None, '', '', '')
+
+    def assertHDFMapBasics(self, _map, _file):
+        # check instance
+        self.assertIsInstance(_map, HDFMap)
+
+        # check attribute existence
+        # attrs = ('_CONTROL_PATH', '_DIGITIZER_PATH', '_MSI_PATH',
+        #          'attrs', 'controls', 'digitizers', 'main_digitizer',
+        #          'msi', 'unknowns')
+        attrs = ('_CONTROL_PATH', '_DIGITIZER_PATH', '_MSI_PATH',
+                 'controls', 'digitizers', 'main_digitizer', 'msi',
+                 'unknowns')
+        for attr in attrs:
+            self.assertTrue(hasattr(_map, attr))
+
+        '''
+        # -- check 'attrs'                                          ----
+        self.assertIsInstance(type(_map).attrs, property)
+        self.assertIsInstance(_map.attrs, dict)
+        self.assertIn('root', _map.attrs)
+        self.assertIsInstance(_map.attrs['root'], dict)
+        for path in (_map._CONTROL_PATH, _map._DIGITIZER_PATH,
+                     _map._MSI_PATH):
+            if path == '/':
+                pass
+            elif path in _file:
+                self.assertIn(path, _map.attrs)
+                self.assertIsInstance(_map.attrs[path], dict)
+
+                for attr in _file[path].attrs:
+                    self.assertIn(attr, _map.attrs[path])
+        '''
+
+        # -- check 'controls'                                       ----
+        self.assertIsInstance(type(_map).controls, property)
+        self.assertIsInstance(_map.controls, (dict, HDFMapControls))
+        for key, val in _map.controls.items():
+            self.assertIsInstance(key, str)
+            self.assertIsInstance(val, HDFMapControlTemplate)
+
+        # -- check 'digitizers'                                     ----
+        self.assertIsInstance(type(_map).digitizers, property)
+        self.assertIsInstance(_map.digitizers, (dict, HDFMapDigitizers))
+        for key, val in _map.digitizers.items():
+            self.assertIsInstance(key, str)
+            self.assertIsInstance(val, HDFMapDigiTemplate)
+
+        # -- check 'main_digitizers'                                ----
+        self.assertIsInstance(type(_map).main_digitizer, property)
+        self.assertIsInstance(_map.main_digitizer,
+                              (type(None), HDFMapDigiTemplate))
+
+        # -- check 'msi'                                            ----
+        self.assertIsInstance(type(_map).msi, property)
+        self.assertIsInstance(_map.msi, (dict, HDFMapMSI))
+        for key, val in _map.msi.items():
+            self.assertIsInstance(key, str)
+            self.assertIsInstance(val, HDFMapMSITemplate)
+
+        # -- check 'unknowns'                                       ----
+        self.assertIsInstance(type(_map).unknowns, property)
+        self.assertIsInstance(_map.unknowns, list)
+        self.assertTrue(all(isinstance(val, str)
+                            for val in _map.unknowns))
 
 
 if __name__ == '__main__':

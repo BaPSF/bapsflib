2f4e49e89f5e3ff4b85f2963051e3e7d45f6d556
rocco8773 <eteverson@gmail.com>
2018-10-22 14:45:32 -0700

    add relative import of `hdfOverview`; remove LaPD specific code from `_build_info`; uncomment method `overview`

--- bapsflib/_hdf/utils/file.py
+++ bapsflib/_hdf/utils/file.py
@@ -12,9 +12,10 @@ import h5py
 import os
 import warnings
 
-# from bapsflib.lapd._hdf.hdfoverview import hdfOverview
 from bapsflib._hdf.maps.hdfmap import HDFMap
 from warnings import warn
+
+from .hdfoverview import hdfOverview
 
 
 class File(h5py.File):
@@ -80,12 +81,6 @@ class File(h5py.File):
             'absolute file path': os.path.abspath(self.filename),
         }
 
-        # add run info
-        # self._info.update(self.file_map.run_info)
-
-        # add exp info
-        # self._info.update(self.file_map.exp_info)
-
     def _map_file(self):
         """Map/re-map the HDF5 file."""
         self._file_map = HDFMap(
@@ -122,15 +117,12 @@ class File(h5py.File):
         """Dictionary of MSI device mappings."""
         return self.file_map.msi
 
-    '''
     @property
     def overview(self):
         """
-        HDF5 file overview. (Instance of
-        :class:`~bapsflib.lapd.hdfoverview.hdfOverview`)
+        HDF5 file overview. (:class:`~.hdfoverview.hdfOverview`)
         """
         return hdfOverview(self)
-    '''
 
     def read_controls(self, controls,
                       shotnum=slice(None),

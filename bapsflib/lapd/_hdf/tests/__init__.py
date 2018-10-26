1bf9f478212946d592314c675016182a5a25eab9
rocco8773 <eteverson@gmail.com>
2018-10-25 21:55:20 -0700

    add a TestBase class for all test classes to subclass

--- bapsflib/lapd/_hdf/tests/__init__.py
+++ bapsflib/lapd/_hdf/tests/__init__.py
@@ -8,3 +8,43 @@
 # License: Standard 3-clause BSD; see "LICENSES/LICENSE.txt" for full
 #   license terms and contributor agreement.
 #
+import unittest as ut
+
+from bapsflib._hdf import File as BaseFile
+from bapsflib._hdf.maps import FauxHDFBuilder
+
+from ..file import File
+
+
+class TestBase(ut.TestCase):
+    """Base test class for all test classes here."""
+
+    f = NotImplemented   # type: FauxHDFBuilder
+
+    @classmethod
+    def setUpClass(cls):
+        # create HDF5 file
+        super().setUpClass()
+        cls.f = FauxHDFBuilder()
+
+    def tearDown(self):
+        self.f.remove_all_modules()
+
+    @classmethod
+    def tearDownClass(cls):
+        # cleanup and close HDF5 file
+        super().tearDownClass()
+        cls.f.cleanup()
+
+    @property
+    def bf(self) -> BaseFile:
+        """Opened BaPSF HDF5 File instance."""
+        return BaseFile(self.f.filename,
+                        control_path='Raw data + config',
+                        digitizer_path='Raw data + config',
+                        msi_path='MSI')
+
+    @property
+    def lapdf(self) -> File:
+        """Opened LaPD HDF5 File instance."""
+        return File(self.f.filename)

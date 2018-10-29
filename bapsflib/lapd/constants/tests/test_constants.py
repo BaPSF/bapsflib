1e43a9c25b9d2397492a4546abe453b2072567b4
rocco8773 <eteverson@gmail.com>
2018-10-29 16:33:50 -0700

    cover class BaPSFConstant

--- bapsflib/lapd/constants/tests/test_constants.py
+++ bapsflib/lapd/constants/tests/test_constants.py
@@ -14,6 +14,7 @@
 import astropy.units as u
 import unittest as ut
 
+from astropy.constants import Constant
 from bapsflib.lapd.constants import (port_spacing, ref_port)
 from bapsflib.lapd.constants.constants import BaPSFConstant
 
@@ -23,10 +24,16 @@ class TestConstants(ut.TestCase):
     Test LaPD constants. (:mod:`bapsflib.lapd.constants.constants`)
     """
 
+    def test_BaPSFConstant(self):
+        self.assertTrue(issubclass(BaPSFConstant, Constant))
+        self.assertEqual(BaPSFConstant.default_reference,
+                         "Basic Plasma Science Facility")
+
     def test_port_spacing(self):
         self.assertIsInstance(port_spacing, BaPSFConstant)
         self.assertEqual(port_spacing.value, 31.95)
         self.assertEqual(port_spacing.unit, u.cm)
+        self.assertEqual(port_spacing.uncertainty, 1.0)
 
     def test_ref_port(self):
         self.assertIsInstance(ref_port, BaPSFConstant)

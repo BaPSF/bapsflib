3ed2fbdc209837ab407101836fe9cdd52a6ed4a1
rocco8773 <eteverson@gmail.com>
2019-04-19 18:44:34 -0700

    only import elementary charge constants `e` and `e_gauss`; convert constants `e`, `e_gauss`, and `u` to BaPSFConstant to provided clearer metadata; update docstring constant table with a 'system' units column

--- bapsflib/plasma/constants.py
+++ bapsflib/plasma/constants.py
@@ -14,18 +14,19 @@ Constants are imported from :mod:`astropy.constants.codata2014`.
 """
 from astropy.constants.codata2014 import (
     c,
-    e, e_gauss, e_emu, e_esu,
+    e, e_gauss,
     eps0,
     g0,
     k_B,
     m_e, m_n, m_p, u,
     mu0,
 )
+from bapsflib.utils import BaPSFConstant
 from numpy import pi
 
 
 __all__ = ['c',
-           'e', 'e_gauss', 'e_emu', 'e_esu',
+           'e', 'e_gauss',
            'eps0',
            'g0',
            'k_B',
@@ -33,38 +34,69 @@ __all__ = ['c',
            'mu0',
            'pi']
 
-'''
-class BaPSFConstant(Constant):
-    """BaPSF Constant"""
-    default_reference = 'Basic Plasma Facility'
-    _registry = {}
-    _has_incompatible_units = set()
-
-
-AMU = BaPSFConstant(**{
-    'abbrev': 'AMU',
-    'name': 'atomic mass unit',
-    'value': const.u.cgs.value,
-    'unit': const.u.cgs.unit,
-    'uncertainty': (const.u.uncertainty * const.u.unit).cgs,
-    'system': 'cgs',
-})
-
-C = BaPSFConstant(**{
-    'abbrev': 'C',
-    'name': 'speed of light in vacuum',
-    'value': const.c.cgs.value,
-    'unit': const.c.cgs.unit,
-    'uncertainty': (const.c.uncertainty * const.c.unit).cgs,
-    'system': 'cgs',
-})
-
+# rename some attributes for clarity
 e = BaPSFConstant(**{
     'abbrev': 'e',
-    'name': 'elementary charge',
-    'value': const.e.cgs.value,
-    'unit': const.e.cgs.unit,
-    'uncertainty': (const.e.uncertainty * const.e.unit).cgs,
-    'system': 'cgs',
+    'name': 'Elementary charge',
+    'value': e.value,
+    'unit': e.unit.name,
+    'uncertainty': e.uncertainty,
+    'system': e.system,
 })
-'''
+e_gauss = BaPSFConstant(**{
+    'abbrev': 'e_gauss',
+    'name': 'Elementary charge',
+    'value': e_gauss.value,
+    'unit': e_gauss.unit.name,
+    'uncertainty': e_gauss.uncertainty,
+    'system': e_gauss.system,
+})
+u = BaPSFConstant(**{
+    'abbrev': 'u',
+    'name': 'Atomic mass unit',
+    'value': u.value,
+    'unit': u.unit.name,
+    'uncertainty': u.uncertainty,
+    'system': u.system,
+})
+
+# clean up
+del BaPSFConstant
+
+# The following code is modified from astropy.constants to produce a
+# table containing information on the constants.
+
+# initialize table
+_tb_div = ((8 * '=') + ' '
+           + (17 * '=') + ' '
+           + (10 * '=') + ' '
+           + (7 * '=') + ' '
+           + (44 * '='))
+_lines = [
+    'The following constants are available:\n',
+    _tb_div,
+    '{0:^8} {1:^17} {2:^10} {3:^7} {4}'.format('Name', 'Value', 'Units',
+                                              'System', 'Description'),
+    _tb_div,
+    '{0:^8} {1:^17.12f} {2:^10} {3:^7} {4}'.format(
+        'pi', pi, '', '',
+        'Ratio of circumference to diameter of circle'),
+]
+
+# add lines to table
+_constants = [eval(item) for item in dir() if item[0] != '_' and item != 'pi']
+_const = None
+for _const in _constants:
+    _lines.append('{0:^8} {1:^17.12g} {2:^10} {3:^7} {4}'
+                  .format(_const.abbrev, _const.value,
+                          _const._unit_string, _const.system,
+                          _const.name))
+
+_lines.append(_lines[1])
+
+# add table to docstrings
+__doc__ += '\n'
+__doc__ += '\n'.join(_lines)
+
+# remove clutter from namespace
+del _const, _constants, _lines, _tb_div,

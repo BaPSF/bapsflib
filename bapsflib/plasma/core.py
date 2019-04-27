656472950bf52a2f59512657e7a77dfc8752da03
rocco8773 <eteverson@gmail.com>
2019-04-26 17:59:31 -0700

    slowly removing code from module, working towards deleting bapsflib/plasma/core.py

--- bapsflib/plasma/core.py
+++ bapsflib/plasma/core.py
@@ -15,14 +15,6 @@
 #
 """Core plasma parameters in (cgs)."""
 
-import astropy.constants as const
-import astropy.units as u
-import math
-
-from scipy import constants
-
-#pconst = constants.physical_constants
-
 '''
 class FloatUnit(float):
     """Template class for floats with a unit attribute."""
@@ -68,89 +60,4 @@ class IntUnit(int):
     def unit(self):
         """units of constant"""
         return self._unit
-'''
-'''
-# ---- length constants ----
-def rce(Bo, kTe, **kwargs):
-    """
-    electron gyroradius (cm)
-
-    .. math::
-
-        r_{ce} = \\frac{v_{T_{e}}}{\Omega_{ce}}
-
-    :param float Bo: magnetic field (in Gauss)
-    :param float kTe: electron temperature (in eV)
-
-    .. note:: see functions :func:`vTe` and :func:`oce`
-    """
-    _rce = vTe(kTe) / abs(oce(Bo))
-    return FloatUnit(_rce, 'cm')
-
-
-def rci(Bo, kTi, m_i, Z, **kwargs):
-    """
-    ion gyroradius (cm)
-
-    .. math::
-
-        r_{ci} = \\frac{v_{T_{i}}}{\Omega_{ci}}
-
-    :param float Bo: magnetic field (in Gauss)
-    :param float kTi: ion temperature (in eV)
-    :param float m_i: ion mass (in g)
-    :param int Z: ion charge number
-
-    .. note:: see functions :func:`vTi` and :func:`oci`
-    """
-    _rci = vTi(kTi, m_i) / oci(Bo, m_i, Z)
-    return FloatUnit(_rci, 'cm')
-
-
-# ---- velocity constants ----
-def VA(Bo, m_i, n_i, **kwargs):
-    """
-    Alfvén Velocity (in cm/s)
-
-    .. math::
-
-        V_{A} = \\frac{B_{o}}{\sqrt{4 \pi n_{i} m_{i}}}
-
-    :param float Bo: magnetic field (in Gauss)
-    :param float m_i: ion mass (in g)
-    :param float n_i: ion number density (in :math:`cm^{-3}`)
-    """
-    _VA = Bo / math.sqrt(4.0 * math.pi * n_i * m_i)
-    return FloatUnit(_VA, 'cm s^-1')
-
-
-def vTe(kTe, **kwargs):
-    """
-    electron thermal velocity (in cm/s)
-
-    .. math::
-
-        v_{T_{e}} = \sqrt{\\frac{k T_{e}}{m_{e}}}
-
-    :param float kTe: electron temperature (in eV)
-    """
-    kTe = kTe * constants.e * 1.e7  # eV to erg
-    _vTe = math.sqrt(kTe / ME)
-    return FloatUnit(_vTe, 'cm s^-1')
-
-
-def vTi(kTi, m_i, **kwargs):
-    """
-    ion thermal velocity (in cm/s)
-
-    .. math::
-
-        v_{T_{i}} = \sqrt{\\frac{k T_{i}}{m_{i}}}
-
-    :param float kTi: ion temperature (in eV)
-    :param float m_i: ion mass (in g)
-    """
-    kTi = kTi * constants.e * 1.e7 # eV to erg
-    _vTi = math.sqrt(kTi / m_i)
-    return FloatUnit(_vTi, 'cm s^-1')
 '''

dae12130caa19e7b84633172e460cd2e493d9990
gbal <gurleenkbal@gmail.com>
2020-06-30 13:23:54 -0700

    This commit includes a working module for converting lapd data array to xarray. Module is called NdaarayToXarray

--- bapsflib/utils/__init__.py
+++ bapsflib/utils/__init__.py
@@ -11,16 +11,24 @@
 """
 Package of developer utilities.
 """
-__all__ = ['decorators', 'errors', 'warnings']
+__all__ = ['decorators', 'errors', 'warnings', 'NdarrayToXarray']
 
 from . import (decorators, errors, warnings)
 
 
-def NdarrayToXarray(data, name={}):
+def NdarrayToXarray(data, arr_name={}):
+    import xarray as xr
     names = list(data.dtype.names)
     names.remove('signal')
-    i=0
-    while i<len(names):
-        print(names[i])
-        i = i+1
-    return x
+    coords_dict = {}
+    for name in names:
+        if name == 'xyz':
+            coords_dict["x"] = ("shot_index", data[name][:, 0])
+            coords_dict["y"] = ("shot_index", data[name][:, 1])
+            coords_dict["z"] = ("shot_index", data[name][:, 2])
+
+        else:
+            coords_dict[name] = ("shot_index", data[name])
+
+    # coords_test={names[0]:("shot_index",data[names[0]]),"x":("shot_index",data[names[1]][:,0]),"y":("shot_index",data[names[1]][:,1]),"z":("shot_index",data[names[1]][:,2]),names[2]:("shot_index",data[names[2]]),names[3]:("shot_index",data[names[3]])}
+    data_array = xr.DataArray(data['signal'], dims=('shot_index', 'time_index'), coords=coords_dict,name=arr_name)

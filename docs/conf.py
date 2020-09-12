7a72a7f4fea8813a5e50199f8f943ab0121618ee
Erik <eteverson@gmail.com>
2020-09-12 11:20:40 -0700

    remove outdated comments

--- docs/conf.py
+++ docs/conf.py
@@ -123,15 +123,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation
 # for a list of builtin themes.
 #
-#html_theme = 'alabaster'
-#html_theme = 'default'
-#on_rtd = os.environ.get('READTHEDOCS') == 'True'
-#if on_rtd:
-#    html_theme = 'default'
-#else:
-#    html_theme = 'sphinx_rtd_theme'
 html_theme = 'sphinx_rtd_theme'
-#html_theme_path = ['_themes']
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see

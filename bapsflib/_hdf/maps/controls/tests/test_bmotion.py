fee91367f65a1924d02838e3918cf3ae4f5440e2
Erik <eteverson@gmail.com>
2025-03-21 11:32:08 -0700

    initial outline of TestBMotion

---
+++ bapsflib/_hdf/maps/controls/tests/test_bmotion.py
@@ -0,0 +1,22 @@
+import unittest as ut
+
+from bapsflib._hdf.maps.controls.bmotion import HDFMapControlBMotion
+from bapsflib._hdf.maps.controls.tests.common import ControlTestCase
+from bapsflib._hdf.maps.controls.types import ConType
+
+
+class TestBMotion(ControlTestCase):
+    """Test class for HDFMapControlBMControl"""
+
+    # define setup variables
+    DEVICE_NAME = "bmotion"
+    DEVICE_PATH = "Raw data + config/bmotion"
+    MAP_CLASS = HDFMapControlBMotion
+    CONTYPE = ConType.MOTION
+
+    def test_raises(self):
+        ...
+
+    @ut.skip("Not implemented")
+    def test_warnings(self):
+        ...

c2833fb86c741810222dd1201d473453dfb41c28
rbuckley-png <rbuckley@physics.ucla.edu>
2025-12-10 13:05:47 -0800

    Functionality to process calibration files for 10 turn BDots.

--- bapsflib/lapd/tools/bdot.py
+++ bapsflib/lapd/tools/bdot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-import numpy as np
-import matplotlib.pyplot as plt
 import argparse
+import matplotlib.pyplot as plt
+import numpy as np
 import os
 
 # =========================

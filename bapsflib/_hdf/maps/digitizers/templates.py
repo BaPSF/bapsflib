1334566e01d8b758bd0c49715ede0a40cbaf3fc1
Erik <eteverson@gmail.com>
2026-03-17 15:33:46 -0700

    remove unnecessary bool(board) and bool(channel) in HDFMapDigiTemplate.get_adc_info()

--- bapsflib/_hdf/maps/digitizers/templates.py
+++ bapsflib/_hdf/maps/digitizers/templates.py
@@ -444,11 +444,11 @@ class HDFMapDigiTemplate(HDFMapTemplate, ABC):
             if board == conn[0]:
                 found = True
                 break
-        if not found or not bool(board):
+        if not found:
             raise ValueError(f"Board number ({board}) not found in setup")
 
         # look for `channel`
-        if channel not in conn[1] or not bool(channel):
+        if channel not in conn[1]:
             raise ValueError(f"Channel number ({channel})  not found in setup")
 
         # get dictionary and add keys

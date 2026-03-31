b35ed90de7aae882e479e68ed0ec21eae22c7c97
Erik <eteverson@gmail.com>
2026-03-30 19:49:15 -0700

    some conf.py cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -178,7 +178,14 @@ language = "en"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "**.ipynb_checkpoints",
+    "common_links.rst",
+    "**Untitled*",
+]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
@@ -186,7 +193,6 @@ pygments_style = "sphinx"
 # If true, `todo` and `todoList` produce output, else they produce
 # nothing.
 todo_include_todos = False
-
 
 # -- Options for HTML output -------------------------------------------
 

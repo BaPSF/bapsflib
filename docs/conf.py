7c5b9ad6da0732f0c6d742885586e9d06d412062
Erik <eteverson@gmail.com>
2026-03-31 19:58:39 -0700

    add sample_phys180E_axial_bdot.hdf5 to exclude_patterns

--- docs/conf.py
+++ docs/conf.py
@@ -178,7 +178,7 @@ language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-# This patterns also effect to html_static_path and html_extra_path
+# These patterns also effect to html_static_path and html_extra_path
 exclude_patterns = [
     "_build",
     "Thumbs.db",
@@ -186,6 +186,7 @@ exclude_patterns = [
     "**.ipynb_checkpoints",
     "common_links.rst",
     "**Untitled*",
+    "notebooks/phys180e/sample_phys180E_axial_bdot.hdf5",
 ]
 
 # The name of the Pygments (syntax highlighting) style to use.

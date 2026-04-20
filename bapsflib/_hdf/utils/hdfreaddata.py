691b790149f9ce37771dbe131fd1b52cb9d14a5d
Erik <eteverson@gmail.com>
2026-04-20 10:37:01 -0700

    remove comment header

--- bapsflib/_hdf/utils/hdfreaddata.py
+++ bapsflib/_hdf/utils/hdfreaddata.py
@@ -1,14 +1,3 @@
-# This file is part of the bapsflib package, a Python toolkit for the
-# BaPSF group at UCLA.
-#
-# http://plasma.physics.ucla.edu/
-#
-# Copyright 2017-2018 Erik T. Everson and contributors
-#
-# License: Standard 3-clause BSD; see "LICENSES/LICENSE.txt" for full
-#   license terms and contributor agreement.
-#
-#
 """
 Module containing the main
 `~bapsflib._hdf.utils.hdfreaddata.HDFReadData` class.

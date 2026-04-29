9b4b5fa46b206e2dcf3de8a1ea71cef114a2cf63
Erik <eteverson@gmail.com>
2026-04-29 10:48:57 -0700

    initialize faux module

---
+++ bapsflib/faux/__init__.py
@@ -0,0 +1,10 @@
+"""
+The `bapsflib.faux` module contains functionality to generate fake /
+faux BaPSF style HDF5 files.
+
+.. caution::
+    The faux functionality is developed to test the `bapsflib` package
+    and help in generating example documnetion.  It is **NOT** intended
+    for end user use.
+
+"""

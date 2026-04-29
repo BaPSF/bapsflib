f24e72cbe633c979531239e4e8125688198574f3
Erik <eteverson@gmail.com>
2026-04-29 11:00:36 -0700

    expand the bapsflib.faux docstring

--- bapsflib/faux/__init__.py
+++ bapsflib/faux/__init__.py
@@ -1,11 +1,23 @@
 """
-The `bapsflib.faux` module contains functionality to generate fake /
-faux BaPSF style HDF5 files.
-
 .. caution::
     The faux functionality is developed to test the `bapsflib` package
     and help in generating example documnetion.  It is **NOT** intended
     for end user use.
+
+The `bapsflib.faux` module contains functionality to generate fake /
+faux BaPSF style HDF5 files.
+
+`bapsflib.faux` is not explicitly exposed to the `bapsflib` namespace,
+so it needs to be explicitly imported.  For example:
+
+>>> import bapsflib
+>>> bapsflib.faux
+AttributeError: module 'bapsflib' has no attribute 'faux'
+>>>
+>>> import bapsflib.faux
+>>> bapsflib.faux
+<module 'bapsflib.faux'>
+
 
 """
 

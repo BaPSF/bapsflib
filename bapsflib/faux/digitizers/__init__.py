2305399fa2c490c6d7c8eec3e9cc3c7ce664a665
Erik <eteverson@gmail.com>
2026-04-30 12:31:58 -0700

    expose all the digitizer Faux classes to the bapsflib.faux.digitizers namespace

---
+++ bapsflib/faux/digitizers/__init__.py
@@ -0,0 +1,13 @@
+"""
+The `bapsflib.faux.digitizers` module contains all the digitzer device
+group generators used by `~bapsflib.faux.builder.FauxHDFBuilder`.
+"""
+__all__ = [
+    "FauxSIS3301",
+    "FauxSISCrate",
+    "FauxLeCroy180E",
+]
+
+from bapsflib.faux.digitizers.lecroy180e import FauxLeCroy180E
+from bapsflib.faux.digitizers.sis3301 import FauxSIS3301
+from bapsflib.faux.digitizers.siscrate import FauxSISCrate
